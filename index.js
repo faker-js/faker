9770d9b95f19f6366a299b0aa3715b7f90f633a2
Matthew Bergman <fotoverite@Fotoverite.local>
2010-05-14 23:50:28 -0400

    finished up name

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ Faker.Address = require('./lib/address');
 var Helper = require('helper');;
 
 sys.puts(JSON.stringify(Faker.Name.first_name()));
+sys.puts(JSON.stringify(Faker.Name.findName()));
 sys.puts(JSON.stringify(Faker.Address.zip_code()));
 sys.puts(JSON.stringify(Faker.Address.secondaryAddress()));
 sys.puts(JSON.stringify(Faker.Address.city()));
-sys.puts(JSON.stringify(Faker.Address.street_name()));
-sys.puts(JSON.stringify(Faker.Address.street_address()));
+sys.puts(JSON.stringify(Faker.Address.streetName()));
+sys.puts(JSON.stringify(Faker.Address.streetAddress()));
 sys.puts(JSON.stringify(Faker.Address.street_address(true)));
 
 
