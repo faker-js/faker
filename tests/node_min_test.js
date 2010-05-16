9f99823a90286ec47323d2b46f814a2f911719af
Marak Squires <marak.squires@gmail.com>
2010-05-16 02:40:32 -0400

    all tests are now card based. started to build out demo page

--- tests/node_min_test.js
+++ tests/node_min_test.js
@@ -1,32 +1,10 @@
 var sys = require('sys');
 
-
 var Faker = require('../Faker');
 
-sys.puts(JSON.stringify(Faker));
+var card = Faker.Helpers.createCard();
 
-sys.puts(JSON.stringify(Faker.Name.findName()));
-sys.puts(JSON.stringify(Faker.Address.zipCode()));
-sys.puts(JSON.stringify(Faker.Address.secondaryAddress()));
-sys.puts(JSON.stringify(Faker.Address.city()));
-sys.puts(JSON.stringify(Faker.Address.streetName()));
-sys.puts(JSON.stringify(Faker.Address.streetAddress()));
-sys.puts(JSON.stringify(Faker.Address.streetAddress(true)));
-sys.puts(JSON.stringify(Faker.Address.ukCountry()));
-sys.puts(JSON.stringify(Faker.Address.ukCounty()));
-sys.puts(JSON.stringify(Faker.PhoneNumber.phoneNumber()));
-sys.puts(JSON.stringify(Faker.Internet.userName()));
-sys.puts(JSON.stringify(Faker.Internet.email()));
-sys.puts(JSON.stringify(Faker.Internet.domainName()));
-sys.puts(JSON.stringify(Faker.Company.companyName()));
-sys.puts(JSON.stringify(Faker.Company.catchPhrase()));
-sys.puts(JSON.stringify(Faker.Company.bs()));
-sys.puts(JSON.stringify(Faker.Lorem.words()));
-sys.puts(JSON.stringify(Faker.Lorem.sentence()));
-sys.puts(JSON.stringify(Faker.Lorem.sentences()));
-sys.puts(JSON.stringify(Faker.Lorem.paragraph()));
-
-
+sys.puts(JSON.stringify(card));
 
 
 
