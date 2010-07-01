f8ec312258428d8b5823ff5b575195fe14c1c8c8
Marak Squires <marak.squires@gmail.com>
2010-07-01 19:54:37 -0400

    regenerated docs again, oops

--- BUILD/docs.js
+++ BUILD/docs.js
@@ -34,7 +34,8 @@
 Heavily inspired by Benjamin Curtis's Ruby Gem [Faker](http://faker.rubyforge.org/) and Perl's [Data::Faker](http://search.cpan.org/~jasonk/Data-Faker-0.07/lib/Data/Faker.pm)
 
           Copyright (c) 2010 Matthew Bergman & Marak Squires http://github.com/marak/Faker.js/
-
+          
+          
           Permission is hereby granted, free of charge, to any person obtaining
           a copy of this software and associated documentation files (the
           "Software"), to deal in the Software without restriction, including
@@ -43,8 +44,10 @@ Heavily inspired by Benjamin Curtis's Ruby Gem [Faker](http://faker.rubyforge.or
           permit persons to whom the Software is furnished to do so, subject to
           the following conditions:
 
+
           The above copyright notice and this permission notice shall be
           included in all copies or substantial portions of the Software.
+
 
           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
           EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
