68d2dc23849d5e9fb62c6a948cbaf5312c05decf
Marak Squires <marak.squires@gmail.com>
2010-07-01 19:57:25 -0400

    i really need a better markdown previewer

--- BUILD/docs.js
+++ BUILD/docs.js
@@ -35,7 +35,6 @@ Heavily inspired by Benjamin Curtis's Ruby Gem [Faker](http://faker.rubyforge.or
 
 <br/>
 Copyright (c) 2010 Matthew Bergman & Marak Squires http://github.com/marak/Faker.js/
-
 <br/>
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -44,11 +43,9 @@ without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
-
 <br/>
 The above copyright notice and this permission notice shall be
 included in all copies or substantial portions of the Software.
-
 <br/>
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
