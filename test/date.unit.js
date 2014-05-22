aae6d9720e77f5f371a865e65eac9cd26626008d
Shane A. Stillwell <shanestillwell@gmail.com>
2014-05-21 21:19:04 -0500

    Ensure Date.future returns a date in the future.

--- test/date.unit.js
+++ test/date.unit.js
@@ -31,6 +31,14 @@ describe("date.js", function () {
             assert.ok(Date.parse(date) > new Date());
         });
 
+        it("returns a future date when N = 0", function () {
+
+            var refDate = new Date();
+            var date = Date.parse(Faker.Date.future(0), refDate.toJSON());
+
+            assert.ok(date > refDate); // date should be after the date given, but before the current time
+        });
+
         it("returns a date N years after the date given", function () {
 
             var refDate = new Date(1880, 11, 9, 10, 0, 0, 0); // set the date beyond the usual calculation (to make sure this is working correctly)
