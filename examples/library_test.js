85ade36275c9f8394ecd0e44fc0f400ffccc5696
Marak <marak.squires@gmail.com>
2015-07-08 15:47:55 -0700

    [dist] Update examples

--- examples/library_test.js
+++
@@ -1,9 +0,0 @@
-var util = require('util');
-
-var definitions = require('../lib/definitions');
-
-var faker = require('../index');
-
-var card = faker.helpers.createCard();
-
-util.puts(JSON.stringify(card));
