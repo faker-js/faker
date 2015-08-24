1c5323061048a164f1671485fda716d1cc10a0d6
Ronen Babayoff <ronen@lavaina.com>
2015-08-23 21:31:27 -0400

    Fix meteor package.js git url

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   summary: "Generate massive amounts of fake data in node.js and in the browser.",
   name: "practicalmeteor:faker",
   version: "2.1.1_1",
-  git: "https://github.com/practicalmeteor/faker.js.git",
+  git: "https://github.com/practicalmeteor/meteor-faker.git",
   documentation: 'README.md'
 });
 
