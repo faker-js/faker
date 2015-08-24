fcdf92ac5d86abaf2f8e6527eae073bbe8da55e0
Ronen Babayoff <ronen@lavaina.com>
2015-08-23 21:28:31 -0400

    Update meteor package.js file to properly point to faker README

--- package.js
+++ package.js
@@ -7,7 +7,8 @@ Package.describe({
   summary: "Generate massive amounts of fake data in node.js and in the browser.",
   name: "practicalmeteor:faker",
   version: "2.1.1_1",
-  git: "https://github.com/practicalmeteor/faker.js.git"
+  git: "https://github.com/practicalmeteor/faker.js.git",
+  documentation: 'README.md'
 });
 
 
