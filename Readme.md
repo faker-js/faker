200ab1321231e63be401917433cd651ba2df7c51
Danielle Adams <danielle.adams@heroku.com>
2018-06-27 20:31:44 -0400

    add commit entry and remove readme changes

--- Readme.md
+++ Readme.md
@@ -129,11 +129,6 @@ This will interpolate the format string with the value of methods `name.lastName
   * ethereumAddress
   * iban
   * bic
-* git
-  * branch
-  * commitSha
-  * commitMessage
-  * shortSha
 * hacker
   * abbreviation
   * adjective
--- test/git.unit.js
+++ test/git.unit.js
@@ -24,10 +24,65 @@ describe("git.js", function() {
     });
   });
 
-  describe("commitSha()", function() {
-    it("returns a random commit SHA", function() {
-      var commitSha = faker.git.commitSha();
-      assert.ok(commitSha.match(/^[a-z0-9]{40}$/));
+  describe("commitEntry()", function() {
+    beforeEach(function() {
+      sinon.spy(faker.git, 'commitMessage');
+      sinon.spy(faker.git, 'commitSha');
+      sinon.spy(faker.internet, 'email');
+      sinon.spy(faker.name, 'firstName');
+      sinon.spy(faker.name, 'lastName');
+      sinon.spy(faker.random, 'number');
+    });
+
+    afterEach(function() {
+      faker.git.commitMessage.restore();
+      faker.git.commitSha.restore();
+      faker.internet.email.restore();
+      faker.name.firstName.restore();
+      faker.name.lastName.restore();
+      faker.random.number.restore();
+    });
+
+    it("returns merge entry at random", function() {
+      faker.git.commitEntry();
+
+      assert.ok(faker.random.number.called);
+    });
+
+    it("returns a commit entry with git commit message and sha", function() {
+      faker.git.commitEntry();
+
+      assert.ok(faker.git.commitMessage.calledOnce);
+      assert.ok(faker.git.commitSha.calledOnce);
+    });
+
+    it("returns a commit entry with internet email", function() {
+      faker.git.commitEntry();
+
+      assert.ok(faker.internet.email.calledOnce);
+    });
+
+    it("returns a commit entry with name first and last", function() {
+      faker.git.commitEntry();
+
+      assert.ok(faker.name.firstName.calledTwice);
+      assert.ok(faker.name.lastName.calledTwice);
+    });
+
+    context("with options['merge'] equal to true", function() {
+      beforeEach(function() {
+        sinon.spy(faker.git, 'shortSha');
+      });
+
+      afterEach(function() {
+        faker.git.shortSha.restore();
+      });
+
+      it("returns a commit entry with merge details", function() {
+        faker.git.commitEntry({ merge: true });
+
+        assert.ok(faker.git.shortSha.calledTwice);
+      });
     });
   });
 
@@ -50,6 +105,14 @@ describe("git.js", function() {
       assert.ok(faker.hacker.verb.calledOnce);
       assert.ok(faker.hacker.adjective.calledOnce);
       assert.ok(faker.hacker.noun.calledOnce);
+    });
+  });
+
+
+  describe("commitSha()", function() {
+    it("returns a random commit SHA", function() {
+      var commitSha = faker.git.commitSha();
+      assert.ok(commitSha.match(/^[a-z0-9]{40}$/));
     });
   });
 
