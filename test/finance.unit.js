b944bbcf991ba9f9d54dab50fc71f64c125546ab
Ash Prosser <aprosser@crowdcomms.co.uk>
2018-09-27 02:12:03 +0100

    fix(bitcoinAddress): fix tests for bitcoin addresses
    The tests for bitcoin addresses had an invalid regex.

--- test/finance.unit.js
+++ test/finance.unit.js
@@ -234,7 +234,12 @@ describe('finance.js', function () {
         it("returns a random bitcoin address", function(){
             var bitcoinAddress = faker.finance.bitcoinAddress();
 
-            assert.ok(bitcoinAddress.match(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/));
+            /**
+             *  Note: Although the total length of a Bitcoin address can be 25-33 characters, regex quantifiers only check the proceding token
+             *  Therefore we take one from the total length of the address not including the first character ([13])
+             */
+
+            assert.ok(bitcoinAddress.match(/^[13][a-km-zA-HJ-NP-Z1-9]{24,33}$/));
         });
     });
 
