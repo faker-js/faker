89edd15c5d0ccb90426e107dd078d9d64ca84399
siwalikm <siwalikm@gmail.com>
2018-11-17 14:02:22 +0530

    adding test cases

--- test/time.unit.js
+++ test/time.unit.js
@@ -8,9 +8,19 @@ describe("time.js", function () {
     describe("recent()", function () {
         it("returns the recent timestamp in Unix time format", function () {
             var date = faker.time.recent();
+            assert.ok(typeof date === 'number');
             assert.ok(date == new Date().getTime());
         });
 
+        it("returns the recent timestamp in full time string format", function () {
+            var date = faker.time.recent('wide');
+            assert.ok(date == new Date().toTimeString());
+        });
+
+        it("returns the recent timestamp in abbreviated string format", function () {
+            var date = faker.time.recent('abbr');
+            assert.ok(date == new Date().toLocaleTimeString());
+        });
     });
 
 });
