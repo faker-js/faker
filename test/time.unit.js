bfe2c9906abd29b7d3d925e6d7cc41530c3a42b8
Marak <marak.squires@gmail.com>
2020-06-11 11:45:17 -0400

    #807 Remove unreliable tests

--- test/time.unit.js
+++ test/time.unit.js
@@ -11,17 +11,19 @@ describe("time.js", function () {
         it("returns the recent timestamp in Unix time format", function () {
             var date = faker.time.recent();
             assert.ok(typeof date === 'number');
-            assert.ok(date == new Date().getTime());
+            // assert.ok(date == new Date().getTime());
         });
 
         it("returns the recent timestamp in full time string format", function () {
             var date = faker.time.recent('wide');
-            assert.ok(date == new Date().toTimeString());
+            assert.ok(typeof date === 'string');
+            // assert.ok(date == new Date().toTimeString());
         });
 
         it("returns the recent timestamp in abbreviated string format", function () {
             var date = faker.time.recent('abbr');
-            assert.ok(date == new Date().toLocaleTimeString());
+            assert.ok(typeof date === 'string');
+            // assert.ok(date == new Date().toLocaleTimeString());
         });
     });
 
