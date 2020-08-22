22f8dba09c8be31e8609ef217d6cf6e27436f3e1
Marak <marak.squires@gmail.com>
2020-08-22 18:20:53 -0400

    Create new gulp tasks using latest gulp APIs

---
+++ build/gulp-tasks/jsdoc.js
@@ -0,0 +1,17 @@
+/*
+
+   this task will generate the jsdoc based HTML documentation found in the /doc/ folder
+
+*/
+
+const { src, dest } = require('gulp');
+const mustache = require('gulp-mustache');
+const rename = require('gulp-rename');
+const jsdoc = require('gulp-jsdoc3');
+
+const config = require('../../conf.json');
+
+module.exports = function jdsoc (cb) {
+  src(['./README.md', './lib/*.js'], { read: false })
+    .pipe(jsdoc(config, cb));
+};
