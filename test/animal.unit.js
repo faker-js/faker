131bef36c122f379f8afd6e6a48a4c3e5a530437
RavsCodes20 <RavsCodes20@users.noreply.github.com>
2021-02-12 20:05:07 -0500

    adding animal module
    started with dogs, added a defintion, an array of dogs breeds, and an associated test.

---
+++ test/animal.unit.js
@@ -0,0 +1,15 @@
+if (typeof module !== 'undefined') {
+  var assert = require('assert');
+  var sinon = require('sinon');
+  var faker = require('../index');
+}
+
+describe("animal.js", function() {
+
+  describe("dog()", function() {
+      it("returns random value from dog array", function() {
+          var dog = faker.animal.dog();
+          assert.ok(faker.definitions.animal.dog.indexOf(animal) !== -1);
+      });
+  });
+}); 
