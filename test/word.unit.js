c0a172dc5d6308731fd879db2ffcddf4714370f5
Adam Walker <adamwalkersemail@gmail.com>
2021-05-19 22:59:53 -0400

    Revert formatting changes to lib/index.js made by prettier

--- test/word.unit.js
+++ test/word.unit.js
@@ -17,12 +17,12 @@ describe.only("word.js", function () {
   // Perform the same three tests for each method.
   methods.forEach(function (method) {
     describe(method + "()", function () {
-      it("returns random value from " +  method + " array", function () {
+      it("returns random value from " + method + " array", function () {
         var word = faker.word[method]();
         assert.ok(faker.definitions.word[method].includes(word));
       });
       it("optional length parameter returns expected result", function () {
-        var wordLength = parseInt(Math.random() * 7 + 3);
+        var wordLength = 5;
         var word = faker.word[method](wordLength);
         assert.ok(faker.definitions.word[method].includes(word));
         assert.ok(word.length == wordLength);
