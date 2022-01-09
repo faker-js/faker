aa930badd82a13c40b453dcfdb0e803846b92d63
Shinigami92 <chrissi92@hotmail.de>
2022-01-09 12:43:20 +0100

    chore: add prettier
    chore: update package lock file

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  plugins: [
+    /* require.resolve("prettier-plugin-organize-imports") */
+  ],
+  singleQuote: true,
+};
