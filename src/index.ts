e0803fcdc914ab770a616da5082d484c64b94922
Eric Jeker <eric.jeker@gmail.com>
2022-01-11 19:26:58 +0800

    Prettify

--- src/index.ts
+++ src/index.ts
@@ -36,24 +36,115 @@ export class Faker {
 
   definitions = {};
   definitionTypes: DefinitionTypes = {
-    "name": ["first_name", "last_name", "prefix", "suffix", "binary_gender", "gender", "title", "male_prefix", "female_prefix", "male_first_name", "female_first_name", "male_middle_name", "female_middle_name", "male_last_name", "female_last_name"],
-    "address": ["city_name", "city_prefix", "city_suffix", "street_suffix", "county", "country", "country_code", "country_code_alpha_3", "state", "state_abbr", "street_prefix", "postcode", "postcode_by_state", "direction", "direction_abbr", "time_zone"],
-    "animal": ["dog", "cat", "snake", "bear", "lion", "cetacean", "insect", "crocodilia", "cow", "bird", "fish", "rabbit", "horse", "type"],
-    "company": ["adjective", "noun", "descriptor", "bs_adjective", "bs_noun", "bs_verb", "suffix"],
-    "lorem": ["words"],
-    "hacker": ["abbreviation", "adjective", "noun", "verb", "ingverb", "phrase"],
-    "phone_number": ["formats"],
-    "finance": ["account_type", "transaction_type", "currency", "iban", "credit_card"],
-    "internet": ["avatar_uri", "domain_suffix", "free_email", "example_email", "password"],
-    "commerce": ["color", "department", "product_name", "price", "categories", "product_description"],
-    "database": ["collation", "column", "engine", "type"],
-    "system": ["mimeTypes", "directoryPaths"],
-    "date": ["month", "weekday"],
-    "vehicle": ["vehicle", "manufacturer", "model", "type", "fuel", "vin", "color"],
-    "music": ["genre"],
-    "word": ["adjective", "adverb", "conjunction", "interjection", "noun", "preposition", "verb"],
-    "title": "",
-    "separator": ""
+    name: [
+      'first_name',
+      'last_name',
+      'prefix',
+      'suffix',
+      'binary_gender',
+      'gender',
+      'title',
+      'male_prefix',
+      'female_prefix',
+      'male_first_name',
+      'female_first_name',
+      'male_middle_name',
+      'female_middle_name',
+      'male_last_name',
+      'female_last_name',
+    ],
+    address: [
+      'city_name',
+      'city_prefix',
+      'city_suffix',
+      'street_suffix',
+      'county',
+      'country',
+      'country_code',
+      'country_code_alpha_3',
+      'state',
+      'state_abbr',
+      'street_prefix',
+      'postcode',
+      'postcode_by_state',
+      'direction',
+      'direction_abbr',
+      'time_zone',
+    ],
+    animal: [
+      'dog',
+      'cat',
+      'snake',
+      'bear',
+      'lion',
+      'cetacean',
+      'insect',
+      'crocodilia',
+      'cow',
+      'bird',
+      'fish',
+      'rabbit',
+      'horse',
+      'type',
+    ],
+    company: [
+      'adjective',
+      'noun',
+      'descriptor',
+      'bs_adjective',
+      'bs_noun',
+      'bs_verb',
+      'suffix',
+    ],
+    lorem: ['words'],
+    hacker: ['abbreviation', 'adjective', 'noun', 'verb', 'ingverb', 'phrase'],
+    phone_number: ['formats'],
+    finance: [
+      'account_type',
+      'transaction_type',
+      'currency',
+      'iban',
+      'credit_card',
+    ],
+    internet: [
+      'avatar_uri',
+      'domain_suffix',
+      'free_email',
+      'example_email',
+      'password',
+    ],
+    commerce: [
+      'color',
+      'department',
+      'product_name',
+      'price',
+      'categories',
+      'product_description',
+    ],
+    database: ['collation', 'column', 'engine', 'type'],
+    system: ['mimeTypes', 'directoryPaths'],
+    date: ['month', 'weekday'],
+    vehicle: [
+      'vehicle',
+      'manufacturer',
+      'model',
+      'type',
+      'fuel',
+      'vin',
+      'color',
+    ],
+    music: ['genre'],
+    word: [
+      'adjective',
+      'adverb',
+      'conjunction',
+      'interjection',
+      'noun',
+      'preposition',
+      'verb',
+    ],
+    title: '',
+    separator: '',
   };
 
   seedValue?: any[] | any;
@@ -67,7 +158,7 @@ export class Faker {
     this.locale = this.locale || opts.locale || 'en';
     this.localeFallback = this.localeFallback || opts.localeFallback || 'en';
 
-    this.loadDefinitions(this.definitionTypes)
+    this.loadDefinitions(this.definitionTypes);
   }
 
   /**
@@ -102,7 +193,7 @@ export class Faker {
               // return localized data
               return this.locales[this.locale][t][p];
             }
-          }
+          },
         });
       });
     });
