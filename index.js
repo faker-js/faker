51fbb752d9d63247b5dcda6122812725cf75caa6
Shinigami92 <chrissi92@hotmail.de>
2022-01-14 19:44:53 +0100

    chore: directly use constructor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 // since we are requiring the top level of faker, load all locales by default
-var lib_1 = require('./lib');
-var faker = new lib_1.Faker({ locales: require('./lib/locales') });
+var Faker = require('./lib');
+var faker = new Faker({ locales: require('./lib/locales') });
 module.exports = faker;
