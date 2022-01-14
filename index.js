175ca8b52b2a6770bb15b89ce88e3d835a25ced3
Shinigami92 <chrissi92@hotmail.de>
2022-01-14 19:22:42 +0100

    chore: single quotes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-"use strict";
+'use strict';
 // since we are requiring the top level of faker, load all locales by default
-var lib_1 = require("./lib");
+var lib_1 = require('./lib');
 var faker = new lib_1.Faker({ locales: require('./lib/locales') });
 module.exports = faker;
