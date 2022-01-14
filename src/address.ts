b9541c8e323771882a23e6d8166d2d0ced3e9b0b
Shinigami92 <chrissi92@hotmail.de>
2022-01-14 17:23:27 +0100

    chore: fix types

--- src/address.ts
+++ src/address.ts
@@ -291,7 +291,7 @@ export class Address {
    * @param useAbbr
    */
   // TODO christopher 2022-01-13: useAbbr not in use
-  state(useAbbr: boolean): string {
+  state(useAbbr?: boolean): string {
     return this.faker.random.arrayElement(this.faker.definitions.address.state);
   }
 
