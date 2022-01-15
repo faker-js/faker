a26074f96e292a616a5fc55365ba87a44809b389
Shinigami92 <chrissi92@hotmail.de>
2022-01-15 10:33:22 +0100

    chore: configure some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,18 @@
 const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
-  ignorePatterns: ['.eslintrc.js', 'lib/'],
+  ignorePatterns: [
+    // Skip self linting
+    '.eslintrc.js',
+
+    // Skip linting generated content
+    'lib/',
+
+    // Skip linting test support files
+    // These may need to be installed via npm dependencies
+    'test/support/',
+  ],
+  root: true,
   env: {
     browser: true,
     mocha: true,
@@ -20,5 +31,34 @@ module.exports = defineConfig({
     warnOnUnsupportedTypeScriptVersion: false,
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/unbound-method': 'warn',
+    'no-useless-escape': 'off', // We may want to use this in the future
+  },
+  overrides: [
+    // Disable some lints for now, until we converted them to typescript
+    {
+      files: ['test/**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['test/**/*.js', 'vendor/*.js'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
+        'prettier/prettier': 'off',
+      },
+    },
+  ],
 });
