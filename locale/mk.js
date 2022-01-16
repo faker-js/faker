8dc69da533a0ccce2493c12261e75a91b8b12bba
Shinigami92 <chrissi92@hotmail.de>
2022-01-16 16:27:35 +0100

    chore: remove old locale folder

--- locale/mk.js
+++
@@ -1,5 +0,0 @@
-var Faker = require('../lib');
-var faker = new Faker({ locale: 'mk', localeFallback: 'en' });
-faker.locales['mk'] = require('../lib/locales/mk');
-faker.locales['en'] = require('../lib/locales/en');
-module['exports'] = faker;
