e97df3c75f4cff737ca6f0688cf1848e22c54a67
Jared Fraser <jared.fraser@gmail.com>
2022-01-17 16:27:19 +1000

    added undefined to type definition

--- src/datatype.ts
+++ src/datatype.ts
@@ -95,7 +95,7 @@ export class Datatype {
    * @method faker.datatype.datetime
    * @param options pass min OR max as number of milliseconds since 1. Jan 1970 UTC
    */
-  datetime(options?: number | { min?: number; max?: number}): Date {
+  datetime(options?: number | { min?: number | undefined; max?: number | undefined}): Date {
     if (typeof options === 'number') {
       options = {
         max: options,
