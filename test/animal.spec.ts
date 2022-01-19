ee44fee5df723926b267f1a1fac2a760d9907fa1
Shinigami92 <chrissi92@hotmail.de>
2022-01-19 23:54:42 +0100

    chore(test): migrate animal

---
+++ test/animal.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { faker } from '../lib';
+
+describe('animal', () => {
+  describe('dog()', () => {
+    it('returns random value from dog array', () => {
+      const dog = faker.animal.dog();
+      expect(faker.definitions.animal.dog).toContain(dog);
+    });
+  });
+});
