7c9b0c3ecb4b8f354a4b6686e48a2d5a05ff92b0
Shinigami92 <chrissi92@hotmail.de>
2022-01-19 23:10:07 +0100

    chore(test): migrate music

---
+++ test/music.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest';
+import { faker } from '../lib';
+
+faker.seed(1234);
+
+describe('music', () => {
+  describe('genre()', () => {
+    it('returns a genre', () => {
+      const genre = faker.music.genre();
+
+      expect(genre).toBe('Electronic');
+    });
+  });
+});
