ec515ec8184d4be5168260433921101350af4272
Shinigami92 <chrissi92@hotmail.de>
2022-01-19 22:35:26 +0100

    chore(test): convert time

---
+++ test/time.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { faker } from '../src';
+
+faker.seed(1234);
+
+describe('time', () => {
+  describe('recent()', () => {
+    it('returns the recent timestamp in Unix time format', () => {
+      const date = faker.time.recent();
+      expect(typeof date).toBe('number');
+    });
+
+    it('returns the recent timestamp in full time string format', () => {
+      const date = faker.time.recent('wide');
+      expect(typeof date).toBe('string');
+    });
+
+    it('returns the recent timestamp in abbreviated string format', () => {
+      const date = faker.time.recent('abbr');
+      expect(typeof date).toBe('string');
+    });
+  });
+});
