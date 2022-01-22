7f3f89b5323fef641db48b74a0e935fd97d57dee
Shinigami92 <chrissi92@hotmail.de>
2022-01-22 19:55:09 +0100

    chore(docs): fix playground

--- docs/.vitepress/theme/components/Playground.vue
+++ docs/.vitepress/theme/components/Playground.vue
@@ -1,18 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import { faker } from '../../../../lib/esm';
 
-const faker = ref();
-const ready = ref(false);
-
-import('../../../../dist/faker').then((_faker) => {
-  window.faker = _faker.default;
-  faker.value = _faker.default;
-  ready.value = true;
-});
+window.faker = faker;
 </script>
 
 <template>
   <h2>🚧 Playground under construction 🚧</h2>
   <h3>window.faker</h3>
-  <pre v-if="ready">{{ Object.keys(faker) }}</pre>
+  <pre>{{ Object.keys(faker) }}</pre>
 </template>
