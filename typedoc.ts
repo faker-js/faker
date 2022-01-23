da582aa1a0c85b2c62ddc7bdc1834dd7edbfb994
Eric Jeker <eric.jeker@gmail.com>
2022-01-23 12:38:51 +0800

    feat: generate markdown

---
+++ typedoc.ts
@@ -0,0 +1,31 @@
+import * as TypeDoc from 'typedoc';
+
+async function main() {
+  const app = new TypeDoc.Application();
+
+  // If you want TypeDoc to load tsconfig.json / typedoc.json files
+  app.options.addReader(new TypeDoc.TSConfigReader());
+  app.options.addReader(new TypeDoc.TypeDocReader());
+
+  app.bootstrap({
+    'plugin': ['typedoc-plugin-missing-exports', 'typedoc-plugin-markdown'],
+    'entryPoints': ['src/index.ts'],
+    'pretty': true,
+  });
+
+  const project = app.convert();
+
+  if (project) {
+    // Project may not have converted correctly
+    const outputDir = 'docs/api';
+
+    // Rendered docs
+    await app.generateDocs(project, outputDir + '/typedoc');
+    // Alternatively generate JSON output
+    await app.generateJson(project, outputDir + '/typedoc.json');
+
+    // Render the Markdown file
+  }
+}
+
+main().catch(console.error);
