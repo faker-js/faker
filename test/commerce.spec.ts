5db164dfea16b819b0d61aa8f2d62649224ee6e2
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 14:53:00 +0100

    chore: format

--- test/commerce.spec.ts
+++ test/commerce.spec.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it } from 'vitest';
 import { faker } from '../dist/cjs';
 
 const seededRuns = [
