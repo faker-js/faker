cedb1ee20c75223ef721e5355f11d905b728660d
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 18:10:37 +0100

    test: use a random generated seed for random-tests

--- test/animal.spec.ts
+++ test/animal.spec.ts
@@ -98,7 +98,10 @@ describe('animal', () => {
     });
   }
 
-  describe('non seed-based tests', () => {
+  // Create and log-back the seed for debug purposes
+  faker.seed(Math.ceil(Math.random() * 1_000_000_000));
+
+  describe(`random seeded tests for seed ${faker.seedValue}`, () => {
     for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
       for (const functionName of functionNames) {
         describe(`${functionName}()`, () => {
