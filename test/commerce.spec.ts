e87415ea581bc1962303caa6814abf66189dbb7b
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 14:50:28 +0100

    test: improve

--- test/commerce.spec.ts
+++ test/commerce.spec.ts
@@ -87,16 +87,27 @@ describe('commerce', () => {
       });
 
       describe(`department()`, () => {
-        it('', () => {
+        it('should return random value from department array', () => {
           const department = faker.commerce.department();
-          expect(department.split(' ')).toHaveLength(1);
+          expect(faker.definitions.commerce.department).toContain(department);
         });
       });
 
       describe(`productName()`, () => {
-        it('', () => {
+        it('should return random values from product arrays', () => {
           const name = faker.commerce.productName();
           expect(name.split(' ').length).greaterThanOrEqual(3);
+
+          const parts = name.split(' ');
+          expect(faker.definitions.commerce.product_name.adjective).toContain(
+            parts[0]
+          );
+          expect(faker.definitions.commerce.product_name.material).toContain(
+            parts[1]
+          );
+          expect(faker.definitions.commerce.product_name.product).toContain(
+            parts[2]
+          );
         });
       });
 
@@ -105,11 +116,10 @@ describe('commerce', () => {
           const price = faker.commerce.price();
 
           expect(price).toBeTruthy();
+
           // TODO @Shinigami92 2022-01-20: I converted the price string to number to satisfy TS
-          expect(+price > 0, 'the amount should be greater than 0').toBe(true);
-          expect(+price < 1001, 'the amount should be less than 1000').toBe(
-            true
-          );
+          expect(+price).greaterThan(0);
+          expect(+price).lessThanOrEqual(1000);
         });
 
         it('should use the default decimal location when not passing arguments', () => {
@@ -117,34 +127,25 @@ describe('commerce', () => {
 
           const decimal = '.';
           const expected = price.length - 3;
+
           const actual = price.indexOf(decimal);
 
           expect(
             actual,
-            'The expected location of the decimal is ' +
-              expected +
-              ' but it was ' +
-              actual +
-              ' amount ' +
-              price
+            `The expected location of the decimal is ${expected} but it was ${actual} amount ${price}`
           ).toBe(expected);
         });
 
         it('should not include a currency symbol by default', () => {
           const amount = faker.commerce.price();
 
-          const regexp = new RegExp(/[0-9.]/);
-
-          const expected = true;
-          const actual = regexp.test(amount);
-
           expect(
-            actual,
+            amount,
             'The expected match should not include a currency symbol'
-          ).toBe(expected);
+          ).match(/[0-9.]/);
         });
 
-        it('it should handle negative amounts, but return 0', () => {
+        it('should handle negative amounts, but return 0', () => {
           const amount = faker.commerce.price(-200, -1);
 
           expect(amount).toBeTruthy();
@@ -152,7 +153,7 @@ describe('commerce', () => {
           expect(+amount === 0.0, 'the amount should equal 0').toBe(true);
         });
 
-        it('it should handle argument dec', () => {
+        it('should handle argument dec', () => {
           const price = faker.commerce.price(100, 100, 1);
 
           expect(price).toBeTruthy();
@@ -161,7 +162,7 @@ describe('commerce', () => {
           );
         });
 
-        it('it should handle argument dec = 0', () => {
+        it('should handle argument dec = 0', () => {
           const price = faker.commerce.price(100, 100, 0);
 
           expect(price).toBeTruthy();
@@ -170,21 +171,38 @@ describe('commerce', () => {
       });
 
       describe(`productAdjective()`, () => {
-        it('', () => {});
+        it('should return random value from product adjective array', () => {
+          const actual = faker.commerce.productAdjective();
+          expect(faker.definitions.commerce.product_name.adjective).toContain(
+            actual
+          );
+        });
       });
 
       describe(`productMaterial()`, () => {
-        it('', () => {});
+        it('should return random value from product material array', () => {
+          const actual = faker.commerce.productMaterial();
+          expect(faker.definitions.commerce.product_name.material).toContain(
+            actual
+          );
+        });
       });
 
       describe(`product()`, () => {
-        it('', () => {});
+        it('should return random value from product array', () => {
+          const actual = faker.commerce.product();
+          expect(faker.definitions.commerce.product_name.product).toContain(
+            actual
+          );
+        });
       });
 
       describe(`productDescription()`, () => {
-        it('should return a random product description', () => {
-          const description = faker.commerce.productDescription();
-          expect(typeof description).toBe('string');
+        it('should return random value from product description array', () => {
+          const actual = faker.commerce.productDescription();
+          expect(faker.definitions.commerce.product_description).toContain(
+            actual
+          );
         });
       });
     }
