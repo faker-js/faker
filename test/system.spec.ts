2b4bd80b39b7662a90c35a1759abf703b0e99eb4
prisis <d.bannert@anolilab.de>
2022-01-25 23:30:31 +0100

    test: remove mocks from system

--- test/system.spec.ts
+++ test/system.spec.ts
@@ -1,74 +1,104 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it } from 'vitest';
 import { faker } from '../dist/cjs';
 
+const seededRuns = [
+  {
+    seed: 42,
+    expectations: {
+      fileName: 'mobile_application.wad',
+      commonFileName: 'mobile_application.gif',
+      mimeType: 'application/vnd.marlin.drm.license+xml',
+      commonFileType: 'audio',
+      commonFileExt: 'png',
+      fileType: 'image',
+      fileExt: 'chm',
+      directoryPath: '/opt/bin',
+      // TODO @prisis 2022-01-25: fix the second ext
+      filePath: '/opt/bin/directives_multi_byte_table.p10.m21',
+      semver: '3.7.9',
+    },
+  },
+  {
+    seed: 1337,
+    expectations: {
+      fileName: 'delaware.vcg',
+      commonFileName: 'delaware.wav',
+      mimeType: 'application/vnd.dxr',
+      commonFileType: 'audio',
+      commonFileExt: 'wav',
+      fileType: 'font',
+      fileExt: 'gxt',
+      directoryPath: '/Library',
+      // TODO @prisis 2022-01-25: fix the second ext
+      filePath: '/Library/bike_kiribati.kpr.ez3',
+      semver: '2.5.1',
+    },
+  },
+  {
+    seed: 1211,
+    expectations: {
+      fileName: 'turnpike_cross_platform_handcrafted.mka',
+      commonFileName: 'turnpike_cross_platform_handcrafted.mp4v',
+      mimeType: 'text/vnd.fmi.flexstor',
+      commonFileType: 'application',
+      commonFileExt: 'htm',
+      fileType: 'x-shader',
+      fileExt: 'opml',
+      directoryPath: '/var/log',
+      // TODO @prisis 2022-01-25: fix the second ext
+      filePath: '/var/log/forward_frozen.swf.fcdt',
+      semver: '9.4.8',
+    },
+  },
+];
+
+const NON_SEEDED_BASED_RUN = 5;
+
+const functionNames = [
+  'fileName',
+  'commonFileName',
+  'mimeType',
+  'commonFileType',
+  'commonFileExt',
+  'fileType',
+  'fileExt',
+  'directoryPath',
+  'filePath',
+  'semver',
+];
+
 describe('system', () => {
-  describe('directoryPath()', () => {
-    it('returns unix fs directory full path', () => {
-      const spy_random_words = vi
-        .spyOn(faker.random, 'words')
-        .mockReturnValue('24/7');
-
-      const directoryPath = faker.system.directoryPath();
-
-      expect(
-        directoryPath.indexOf('/'),
-        'generated directoryPath should start with /'
-      ).toBe(0);
-
-      spy_random_words.mockRestore();
-    });
+  afterEach(() => {
+    faker.locale = 'en';
   });
 
-  describe('filePath()', () => {
-    it('returns unix fs file full path', () => {
-      const spy_random_words = vi
-        .spyOn(faker.random, 'words')
-        .mockReturnValue('24/7');
+  for (let { seed, expectations } of seededRuns) {
+    describe(`seed: ${seed}`, () => {
+      for (const functionName of functionNames) {
+        it(`${functionName}()`, () => {
+          faker.seed(seed);
 
-      const filePath = faker.system.filePath();
-
-      expect(
-        filePath.indexOf('/'),
-        'generated filePath should start with /'
-      ).toBe(0);
-
-      spy_random_words.mockRestore();
+          const actual = faker.system[functionName]();
+          expect(actual).toEqual(expectations[functionName]);
+        });
+      }
     });
-  });
+  }
 
-  describe('fileName()', () => {
-    it('returns filenames without system path separators', () => {
-      const spy_random_words = vi
-        .spyOn(faker.random, 'words')
-        .mockReturnValue('24/7');
+  // Create and log-back the seed for debug purposes
+  faker.seed(Math.ceil(Math.random() * 1_000_000_000));
 
-      const fileName = faker.system.fileName();
+  describe(`random seeded tests for seed ${faker.seedValue}`, () => {
+    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
+      for (const functionName of functionNames) {
+        describe(`${functionName}()`, () => {
+          it(`should return random value from ${functionName} array`, () => {
+            const actual = faker.system[functionName]();
 
-      expect(
-        fileName.indexOf('/'),
-        'generated fileNames should not have path separators'
-      ).toBe(-1);
-
-      spy_random_words.mockRestore();
-    });
-  });
-
-  describe('commonFileName()', () => {
-    it('returns filenames without system path separators', () => {
-      const spy_random_words = vi
-        .spyOn(faker.random, 'words')
-        .mockReturnValue('24/7');
-
-      const fileName =
-        // @ts-expect-error
-        faker.system.commonFileName();
-
-      expect(
-        fileName.indexOf('/'),
-        'generated commonFileNames should not have path separators'
-      ).toBe(-1);
-
-      spy_random_words.mockRestore();
-    });
+            expect(actual !== '').toBeTruthy();
+          });
+        });
+      }
+    }
   });
 });
