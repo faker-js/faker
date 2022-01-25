238afa76bf8d1417792965b853e2f8f9ed18ea6b
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 14:31:11 +0100

    test: rewrite commerce tests

--- test/commerce.spec.ts
+++ test/commerce.spec.ts
@@ -1,158 +1,192 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { faker } from '../dist/cjs';
 
+const seededRuns = [
+  {
+    seed: 42,
+    expectations: {
+      color: 'grey',
+      department: 'Tools',
+      productName: 'Fantastic Soft Sausages',
+      price: '375.00',
+      productAdjective: 'Fantastic',
+      productMaterial: 'Cotton',
+      product: 'Pants',
+      productDescription:
+        'The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design',
+    },
+  },
+  {
+    seed: 1337,
+    expectations: {
+      color: 'black',
+      department: 'Computers',
+      productName: 'Gorgeous Rubber Keyboard',
+      price: '263.00',
+      productAdjective: 'Gorgeous',
+      productMaterial: 'Concrete',
+      product: 'Ball',
+      productDescription:
+        'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+    },
+  },
+  {
+    seed: 1211,
+    expectations: {
+      color: 'azure',
+      department: 'Automotive',
+      productName: 'Unbranded Granite Salad',
+      price: '929.00',
+      productAdjective: 'Unbranded',
+      productMaterial: 'Frozen',
+      product: 'Sausages',
+      productDescription:
+        'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals',
+    },
+  },
+];
+
+const NON_SEEDED_BASED_RUN = 5;
+
+const functionNames = [
+  'color',
+  'department',
+  'productName',
+  'price',
+  'productAdjective',
+  'productMaterial',
+  'product',
+  'productDescription',
+];
+
 describe('commerce', () => {
-  describe('color()', () => {
-    it('returns random value from commerce.color array', () => {
-      const color = faker.commerce.color();
-      expect(faker.definitions.commerce.color).toContain(color);
-    });
+  afterEach(() => {
+    faker.locale = 'en';
   });
 
-  describe('department(max, fixedValue)', () => {
-    it('should use the default amounts when not passing arguments', () => {
-      const department = faker.commerce.department();
-      expect(department.split(' ')).toHaveLength(1);
+  for (let { seed, expectations } of seededRuns) {
+    describe(`seed: ${seed}`, () => {
+      for (const functionName of functionNames) {
+        it(`${functionName}()`, () => {
+          faker.seed(seed);
+
+          const actual = faker.commerce[functionName]();
+          expect(actual).toEqual(expectations[functionName]);
+        });
+      }
     });
+  }
 
-    /*
+  describe('non seed-based tests', () => {
+    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
+      describe(`color()`, () => {
+        it('should return random value from color array', () => {
+          const actual = faker.commerce.color();
+          expect(faker.definitions.commerce.color).toContain(actual);
+        });
+      });
 
-    it("should return only one value if we specify a maximum of one", function() {
-        sinon.spy(faker.random, 'arrayElement');
+      describe(`department()`, () => {
+        it('', () => {
+          const department = faker.commerce.department();
+          expect(department.split(' ')).toHaveLength(1);
+        });
+      });
 
-        const department = faker.commerce.department(1);
+      describe(`productName()`, () => {
+        it('', () => {
+          const name = faker.commerce.productName();
+          expect(name.split(' ').length).greaterThanOrEqual(3);
+        });
+      });
 
-        assert.strictEqual(department.split(" ").length, 1);
-        assert.ok(faker.random.arrayElement.calledOnce);
+      describe(`price()`, () => {
+        it('should use the default amounts when not passing arguments', () => {
+          const price = faker.commerce.price();
 
-        faker.random.arrayElement.restore();
-    });
+          expect(price).toBeTruthy();
+          // TODO @Shinigami92 2022-01-20: I converted the price string to number to satisfy TS
+          expect(+price > 0, 'the amount should be greater than 0').toBe(true);
+          expect(+price < 1001, 'the amount should be less than 1000').toBe(
+            true
+          );
+        });
 
-    it("should return the maximum value if we specify the fixed value", function() {
-        sinon.spy(faker.random, 'arrayElement');
+        it('should use the default decimal location when not passing arguments', () => {
+          const price = faker.commerce.price();
 
-        const department = faker.commerce.department(5, true);
+          const decimal = '.';
+          const expected = price.length - 3;
+          const actual = price.indexOf(decimal);
 
-        console.log(department);
+          expect(
+            actual,
+            'The expected location of the decimal is ' +
+              expected +
+              ' but it was ' +
+              actual +
+              ' amount ' +
+              price
+          ).toBe(expected);
+        });
 
-        // account for the separator
-        assert.strictEqual(department.split(" ").length, 6);
-        // Sometimes it will generate duplicates that aren't used in the final string,
-        // so we check if arrayElement has been called exactly or more than 5 times
-        assert.ok(faker.random.arrayElement.callCount >= 5);
+        it('should not include a currency symbol by default', () => {
+          const amount = faker.commerce.price();
 
-        faker.random.arrayElement.restore();
-    });
-    */
-  });
+          const regexp = new RegExp(/[0-9.]/);
 
-  describe('productName()', () => {
-    it('returns name comprising of an adjective, material and product', () => {
-      const spy_random_arrayElement = vi.spyOn(faker.random, 'arrayElement');
-      const spy_commerce_productAdjective = vi.spyOn(
-        faker.commerce,
-        'productAdjective'
-      );
-      const spy_commerce_productMaterial = vi.spyOn(
-        faker.commerce,
-        'productMaterial'
-      );
-      const spy_commerce_product = vi.spyOn(faker.commerce, 'product');
+          const expected = true;
+          const actual = regexp.test(amount);
 
-      const name = faker.commerce.productName();
+          expect(
+            actual,
+            'The expected match should not include a currency symbol'
+          ).toBe(expected);
+        });
 
-      expect(name.split(' ').length).greaterThanOrEqual(3);
+        it('it should handle negative amounts, but return 0', () => {
+          const amount = faker.commerce.price(-200, -1);
 
-      expect(spy_random_arrayElement).toHaveBeenCalledTimes(3);
-      expect(spy_commerce_productAdjective).toHaveBeenCalledOnce();
-      expect(spy_commerce_productMaterial).toHaveBeenCalledOnce();
-      expect(spy_commerce_product).toHaveBeenCalledOnce();
+          expect(amount).toBeTruthy();
+          // TODO @Shinigami92 2022-01-20: I converted the price string to number to satisfy TS
+          expect(+amount === 0.0, 'the amount should equal 0').toBe(true);
+        });
 
-      spy_random_arrayElement.mockRestore();
-      spy_commerce_productAdjective.mockRestore();
-      spy_commerce_productMaterial.mockRestore();
-      spy_commerce_product.mockRestore();
-    });
-  });
+        it('it should handle argument dec', () => {
+          const price = faker.commerce.price(100, 100, 1);
 
-  describe('price(min, max, dec, symbol)', () => {
-    it('should use the default amounts when not passing arguments', () => {
-      const price = faker.commerce.price();
+          expect(price).toBeTruthy();
+          expect(price, 'the price should be equal 100.0').toStrictEqual(
+            '100.0'
+          );
+        });
 
-      expect(price).toBeTruthy();
-      // TODO @Shinigami92 2022-01-20: I converted the price string to number to satisfy TS
-      expect(+price > 0, 'the amount should be greater than 0').toBe(true);
-      expect(+price < 1001, 'the amount should be less than 1000').toBe(true);
-    });
+        it('it should handle argument dec = 0', () => {
+          const price = faker.commerce.price(100, 100, 0);
 
-    it('should use the default decimal location when not passing arguments', () => {
-      const price = faker.commerce.price();
+          expect(price).toBeTruthy();
+          expect(price, 'the price should be equal 100').toStrictEqual('100');
+        });
+      });
 
-      const decimal = '.';
-      const expected = price.length - 3;
-      const actual = price.indexOf(decimal);
+      describe(`productAdjective()`, () => {
+        it('', () => {});
+      });
 
-      expect(
-        actual,
-        'The expected location of the decimal is ' +
-          expected +
-          ' but it was ' +
-          actual +
-          ' amount ' +
-          price
-      ).toBe(expected);
-    });
+      describe(`productMaterial()`, () => {
+        it('', () => {});
+      });
 
-    it('should not include a currency symbol by default', () => {
-      const amount = faker.commerce.price();
+      describe(`product()`, () => {
+        it('', () => {});
+      });
 
-      const regexp = new RegExp(/[0-9.]/);
-
-      const expected = true;
-      const actual = regexp.test(amount);
-
-      expect(
-        actual,
-        'The expected match should not include a currency symbol'
-      ).toBe(expected);
-    });
-
-    it('it should handle negative amounts, but return 0', () => {
-      const amount = faker.commerce.price(-200, -1);
-
-      expect(amount).toBeTruthy();
-      // TODO @Shinigami92 2022-01-20: I converted the price string to number to satisfy TS
-      expect(+amount === 0.0, 'the amount should equal 0').toBe(true);
-    });
-
-    it('it should handle argument dec', () => {
-      const price = faker.commerce.price(100, 100, 1);
-
-      expect(price).toBeTruthy();
-      expect(price, 'the price should be equal 100.0').toStrictEqual('100.0');
-    });
-
-    it('it should handle argument dec = 0', () => {
-      const price = faker.commerce.price(100, 100, 0);
-
-      expect(price).toBeTruthy();
-      expect(price, 'the price should be equal 100').toStrictEqual('100');
-    });
-  });
-
-  describe('productDescription()', () => {
-    it('returns a random product description', () => {
-      const spy_commerce_productDescription = vi.spyOn(
-        faker.commerce,
-        'productDescription'
-      );
-
-      const description = faker.commerce.productDescription();
-
-      expect(typeof description).toBe('string');
-      expect(spy_commerce_productDescription).toHaveBeenCalledOnce();
-
-      spy_commerce_productDescription.mockRestore();
-    });
+      describe(`productDescription()`, () => {
+        it('should return a random product description', () => {
+          const description = faker.commerce.productDescription();
+          expect(typeof description).toBe('string');
+        });
+      });
+    }
   });
 });
