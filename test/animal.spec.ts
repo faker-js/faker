12d4359cc77b0dc7858818bfc53976a6ea8cb89a
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 10:00:06 +0100

    test: improve animal tests

--- test/animal.spec.ts
+++ test/animal.spec.ts
@@ -1,11 +1,36 @@
 import { describe, expect, it } from 'vitest';
 import { faker } from '../dist/cjs';
 
+const NON_SEEDED_BASED_RUN = 5;
+
+const functionNames = [
+  'dog',
+  'cat',
+  'snake',
+  'bear',
+  'lion',
+  'cetacean',
+  'horse',
+  'bird',
+  'cow',
+  'fish',
+  'crocodilia',
+  'insect',
+  'rabbit',
+  'type',
+];
+
 describe('animal', () => {
-  describe('dog()', () => {
-    it('returns random value from dog array', () => {
-      const dog = faker.animal.dog();
-      expect(faker.definitions.animal.dog).toContain(dog);
-    });
+  describe('non seed-based tests', () => {
+    for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
+      for (const functionName of functionNames) {
+        describe(`${functionName}()`, () => {
+          it(`should return random value from ${functionName} array`, () => {
+            const actual = faker.animal[functionName]();
+            expect(faker.definitions.animal[functionName]).toContain(actual);
+          });
+        });
+      }
+    }
   });
 });
