9773f7de78e0eccf7e3730bf506b8d559f514591
Jessica Sachs <jess@jessicasachs.io>
2022-01-28 23:00:22 -0500

    fix: removing toc to fix html for codegen'd docs

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,11 +1,25 @@
 describe('Smoke Test', () => {
-  it('compiles the guide page', () => {
+  beforeEach(() => {
     cy.visit('/guide/');
+    cy.get('a[href="/api/animal.html"]').as('firstSectionLink');
+  });
+  it('compiles the guide page', () => {
     cy.contains('Getting Started');
   });
 
-  // it('compiles the playground page', () => {
-  //   cy.visit('/playground/');
-  //   cy.contains('Playground under construction');
-  // });
+  it('renders this last code example in the code', () => {
+    // Click on any section in the sidebar
+    cy.get('@firstSectionLink')
+      .click()
+      // Make sure the number of code examples is the same between reloads
+      .get('.container pre code')
+      .then(($codeBlocks) => {
+        // Trigger a reload
+        cy.reload()
+          // Give the runtime a chance to update/fail
+          .wait(500)
+          .get('.container pre code')
+          .should('have.length', $codeBlocks.length);
+      });
+  });
 });
