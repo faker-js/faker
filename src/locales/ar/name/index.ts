c6eb25f606633886fce8a88a36f06c4270414b34
in-ceptive <dev@in-ceptive.com>
2022-01-30 01:45:20 +0400

    feat: add more arabic names datasets

--- src/locales/ar/name/index.ts
+++ src/locales/ar/name/index.ts
@@ -1,14 +1,20 @@
 import first_name from './first_name';
 import last_name from './last_name';
+import male_first_name from './male_first_name';
+import female_first_name from './female_first_name';
 import name_ from './name';
 import prefix from './prefix';
+import suffix from './suffix';
 import title from './title';
 
 const name: any = {
+  male_first_name,
+  female_first_name,
   first_name,
   last_name,
   name: name_,
   prefix,
+  suffix,
   title,
 };
 
