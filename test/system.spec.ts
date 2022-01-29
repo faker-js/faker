02b59f7d5cdba998597296f78fd8370882fc816a
Daniel Bannert <prisis@users.noreply.github.com>
2022-01-29 13:23:23 +0100

    Update test/system.spec.ts
    Co-authored-by: Shinigami <chrissi92@hotmail.de>

--- test/system.spec.ts
+++ test/system.spec.ts
@@ -72,7 +72,7 @@ describe('system', () => {
     faker.locale = 'en';
   });
 
-  for (let { seed, expectations } of seededRuns) {
+  for (const { seed, expectations } of seededRuns) {
     describe(`seed: ${seed}`, () => {
       for (const functionName of functionNames) {
         it(`${functionName}()`, () => {
