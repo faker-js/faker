8865608c7a0c840c7bdce062595d5e6e0c3b2405
ST-DDT <ST-DDT@gmx.de>
2022-01-30 17:29:51 +0100

    feat: split database to its own file

--- src/locales/en/database/index.ts
+++ src/locales/en/database/index.ts
@@ -1,9 +1,10 @@
+import type { DatabaseDefinition } from '../../../definitions';
 import collation from './collation';
 import column from './column';
 import engine from './engine';
 import type from './type';
 
-const database: any = {
+const database: DatabaseDefinition = {
   collation,
   column,
   engine,
