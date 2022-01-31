4b959ee626b0c3b778a6158639a3d6f1deb685bd
Piotr Narożnik <pnaroznik.dev@gmail.com>
2022-01-31 23:01:10 +0100

    test: rewrite music tests

--- test/music.spec.ts
+++ test/music.spec.ts
@@ -1,14 +1,60 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { faker } from '../dist/cjs';
 
-faker.seed(1234);
+const seededRuns = [
+  {
+    seed: 42,
+    expectations: {
+      genre: {
+        noArgs: 'Country',
+      },
+    },
+  },
+  {
+    seed: 1337,
+    expectations: {
+      genre: {
+        noArgs: 'Folk',
+      },
+    },
+  },
+  {
+    seed: 1211,
+    expectations: {
+      genre: {
+        noArgs: 'Non Music',
+      },
+    },
+  },
+];
+
+const functionNames = ['genre'];
 
 describe('music', () => {
+  beforeEach(() => {
+    faker.locale = 'en';
+  });
+
+  for (const { seed, expectations } of seededRuns) {
+    describe(`seed: ${seed}`, () => {
+      for (const functionName of functionNames) {
+        it(`${functionName}()`, () => {
+          faker.seed(seed);
+
+          const actual = faker.music[functionName]();
+
+          expect(actual).toEqual(expectations[functionName].noArgs);
+        });
+      }
+    });
+  }
+
   describe('genre()', () => {
-    it('returns a genre', () => {
+    it('should return a genre', () => {
       const genre = faker.music.genre();
 
-      expect(genre).toBe('Electronic');
+      expect(typeof genre).toBe('string');
+      expect(genre).toBeTruthy();
     });
   });
 });
