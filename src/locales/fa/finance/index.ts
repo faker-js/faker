5e6754da61b63019fd063fad26adbeeabd8b789b
ST-DDT <ST-DDT@gmx.de>
2022-02-01 17:31:52 +0100

    feat(types): provide strong typing for locales (#363)

--- src/locales/fa/finance/index.ts
+++ src/locales/fa/finance/index.ts
@@ -1,9 +1,10 @@
+import type { FinanceDefinitions } from '../../../definitions';
 import account_type from './account_type';
 import credit_card from './credit_card';
 import currency from './currency';
 import transaction_type from './transaction_type';
 
-const finance: any = {
+const finance: FinanceDefinitions = {
   account_type,
   credit_card,
   currency,
