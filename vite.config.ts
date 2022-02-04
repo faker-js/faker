03894849c1a884579ec1f91530a7e4ae06cc38d5
Shinigami92 <chrissi92@hotmail.de>
2022-02-04 23:47:32 +0100

    ci: try something else

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ export default defineConfig({
   test: {
     coverage: {
       all: true,
-      reporter: ['lcov'],
-      include: ['dist/cjs'],
+      reporter: ['clover', 'cobertura', 'lcov', 'text'],
+      // TODO christopher 2022-02-04: Later we will only cover `src` instead of `dist` and `src`
+      include: ['dist', 'src'],
     },
   },
 });
