6a259c5dd329b44811866e285769985e59c8f256
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-02-21 19:03:04 +0100

    fix: MersenneTwister19937 methode scoping

--- src/vendor/mersenne.ts
+++ src/vendor/mersenne.ts
@@ -295,51 +295,51 @@ class MersenneTwister19937 {
    * Generates a random number on [0,2^32]-interval
    */
   //c//long genrand_int31(void)
-  genrandInt31 = function (): number {
+  genrandInt31(): number {
     //c//return (genrand_int32()>>1);
-    return this.genrand_int32() >>> 1;
-  };
+    return this.genrandInt32() >>> 1;
+  }
 
   /**
    * Generates a random number on [0,1]-real-interval
    */
   //c//double genrand_real1(void)
-  genrandReal1 = function (): number {
+  genrandReal1(): number {
     //c//return genrand_int32()*(1.0/4294967295.0);
-    return this.genrand_int32() * (1.0 / 4294967295.0);
+    return this.genrandInt32() * (1.0 / 4294967295.0);
     /* divided by 2^32-1 */
-  };
+  }
 
   /**
    * Generates a random number on [0,1)-real-interval
    */
   //c//double genrand_real2(void)
-  genrandReal2 = function (): number {
+  genrandReal2(): number {
     //c//return genrand_int32()*(1.0/4294967296.0);
-    return this.genrand_int32() * (1.0 / 4294967296.0);
+    return this.genrandInt32() * (1.0 / 4294967296.0);
     /* divided by 2^32 */
-  };
+  }
 
   /**
    * Generates a random number on (0,1)-real-interval
    */
   //c//double genrand_real3(void)
-  genrandReal3 = function (): number {
+  genrandReal3(): number {
     //c//return ((genrand_int32()) + 0.5)*(1.0/4294967296.0);
-    return ((this.genrand_int32() as number) + 0.5) * (1.0 / 4294967296.0);
+    return (this.genrandInt32() + 0.5) * (1.0 / 4294967296.0);
     /* divided by 2^32 */
-  };
+  }
 
   /**
    * Generates a random number on [0,1) with 53-bit resolution
    */
   //c//double genrand_res53(void)
-  genrandRes53 = function (): number {
+  genrandRes53(): number {
     //c//unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6;
-    const a = this.genrand_int32() >>> 5,
-      b = this.genrand_int32() >>> 6;
+    const a = this.genrandInt32() >>> 5,
+      b = this.genrandInt32() >>> 6;
     return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
-  };
+  }
   /* These real versions are due to Isaku Wada, 2002/01/09 added */
 }
 
