754b7c472562dbcee2b5bacef04c08fcb806b12e
Leyla Jähnig <77127505+xDivisionByZerox@users.noreply.github.com>
2022-03-29 18:17:03 +0200

    docs(errors.faker): add missing dot
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/errors/faker.error.ts
+++ src/errors/faker.error.ts
@@ -1,5 +1,5 @@
 /**
- * An error instance that will be thrown by faker
+ * An error instance that will be thrown by faker.
  */
 export class FakerError extends Error {
   constructor(message: string) {
