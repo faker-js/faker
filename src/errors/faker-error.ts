36627bb725034c85a542999854e17a2dcc628a78
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-03-30 08:45:23 +0200

    refactor(FakerError): simplify class definition

--- src/errors/faker-error.ts
+++ src/errors/faker-error.ts
@@ -1,8 +1,4 @@
 /**
  * An error instance that will be thrown by faker.
  */
-export class FakerError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class FakerError extends Error {}
