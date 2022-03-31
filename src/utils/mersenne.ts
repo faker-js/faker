9789c41417da45c4ba9ef3f3f5e0b400fa735394
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2022-03-31 18:47:57 +0200

    docs: format to 80 columns and convert to jsdoc format

--- src/utils/mersenne.ts
+++ src/utils/mersenne.ts
@@ -1,61 +1,71 @@
-// Copyright (c) 2022 Faker
-//
-// This is a version of the original source code migrated to TypeScript and modified by the Faker team.
-// Check LICENSE for more details on copyright.
-//
-// ===
-//
-// this program is a JavaScript version of Mersenne Twister, with concealment and encapsulation in class,
-// an almost straight conversion from the original program, mt19937ar.c,
-// translated by y. okada on July 17, 2006.
-// and modified a little at july 20, 2006, but there are not any substantial differences.
-// in this program, procedure descriptions and comments of original source code were not removed.
-// lines commented with //c// were originally descriptions of c procedure. and a few following lines are appropriate JavaScript descriptions.
-// lines commented with /* and */ are original comments.
-// lines commented with // are additional comments in this JavaScript version.
-// before using this version, create at least one instance of MersenneTwister19937 class, and initialize the each state, given below in c comments, of all the instances.
-//
-// ===
-
-/*
- *   A C-program for MT19937, with initialization improved 2002/1/26.
- *   Coded by Takuji Nishimura and Makoto Matsumoto.
+/**
+ * Copyright (c) 2022 Faker
  *
- *   Before using, initialize the state by using init_genrand(seed)
- *   or init_by_array(init_key, key_length).
+ * This is a version of the original source code migrated to TypeScript and
+ * modified by the Faker team.
  *
- *   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
- *   All rights reserved.
+ * Check LICENSE for more details on copyright.
  *
- *   Redistribution and use in source and binary forms, with or without
- *   modification, are permitted provided that the following conditions
- *   are met:
+ * -----------------------------------------------------------------------------
  *
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
+ * Copyright (c) 2006 Y. Okada
  *
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
+ * This program is a JavaScript version of Mersenne Twister, with concealment
+ * and encapsulation in class, an almost straight conversion from the original
+ * program, mt19937ar.c, translated by Y. Okada on July 17, 2006, and modified
+ * a little at July 20, 2006, but there are not any substantial differences.
  *
- *     3. The names of its contributors may not be used to endorse or promote
- *        products derived from this software without specific prior written
- *        permission.
+ * In this program, procedure descriptions and comments of original source code
+ * were not removed.
  *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Lines commented with //c// were originally descriptions of c procedure.
+ * And a few following lines are appropriate JavaScript descriptions.
  *
+ * Lines commented with /* and *\/ are original comments.
+ * Lines commented with // are additional comments in this JavaScript version.
  *
- *   Any feedback is very welcome.
+ * Before using this version, create at least one instance of
+ * MersenneTwister19937 class, and initialize the each state, given below
+ * in C comments, of all the instances.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * A C-program for MT19937, with initialization improved 2002/1/26.
+ * Coded by Takuji Nishimura and Makoto Matsumoto.
+ *
+ * Before using, initialize the state by using init_genrand(seed)
+ * or init_by_array(init_key, key_length).
+ *
+ * Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The names of its contributors may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Any feedback is very welcome.
  *   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
  *   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
  */
@@ -81,7 +91,7 @@ export default class MersenneTwister19937 {
   private mti = this.N + 1; /* mti==N+1 means mt[N] is not initialized */
 
   /**
-   * Returns a 32-bits unsiged integer from an operand to which applied a bit operator.
+   * Returns a 32-bits unsigned integer from an operand to which applied a bit operator.
    *
    * @param n1 number to unsign
    */
@@ -91,7 +101,7 @@ export default class MersenneTwister19937 {
   }
 
   /**
-   * Emulates lowerflow of a c 32-bits unsiged integer variable, instead of the operator -.
+   * Emulates lowerflow of a c 32-bits unsigned integer variable, instead of the operator -.
    * These both arguments must be non-negative integers expressible using unsigned 32 bits.
    *
    * @param n1 dividend
