5e73fc3aa38946c59a90975f14b15d1ebc3d8a5c
Shinigami92 <chrissi92@hotmail.de>
2022-05-03 16:07:07 +0200

    fix: hu locale

--- src/locale/hu.ts
+++ src/locale/hu.ts
@@ -7,7 +7,7 @@ import { Faker } from '../faker';
 import en from '../locales/en';
 import hu from '../locales/hu';
 
-const faker = new Faker({
+export const faker = new Faker({
   locale: 'hu',
   localeFallback: 'en',
   locales: {
@@ -15,5 +15,3 @@ const faker = new Faker({
     en,
   },
 });
-
-export = faker;
