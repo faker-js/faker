ccec7887f88e4f1e3fa2c2cd833769771c4a6775
ST-DDT <ST-DDT@gmx.de>
2022-05-03 09:46:21 +0200

    chore: cleanup and comments

--- test/scripts/apidoc/examplesAndDeprecations.spec.ts
+++ test/scripts/apidoc/examplesAndDeprecations.spec.ts
@@ -8,7 +8,7 @@ import { resolve } from 'path';
 import type { DeclarationReflection, SignatureReflection } from 'typedoc';
 import { ReflectionKind } from 'typedoc';
 import type { SpyInstance } from 'vitest';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { faker } from '../../../src';
 import { loadProject } from './utils';
 
@@ -42,9 +42,10 @@ describe('examples and deprecations', () => {
     .filter((key) => typeof console[key] === 'function')
     .map((methodName) => vi.spyOn(console, methodName as keyof typeof console));
 
-  afterEach(() => {
+  afterAll(() => {
+    faker.locale = 'en';
     for (const spy of consoleSpies) {
-      spy.mockReset();
+      spy.mockRestore();
     }
   });
 
@@ -67,32 +68,35 @@ describe('examples and deprecations', () => {
         method.signatures || method.type['declaration'].signatures;
       const signature = signatures[signatures.length - 1];
 
-      const examples =
+      // Extract examples and make them runnable
+      let examples =
         signature?.comment?.tags
           .filter((tag) => tag.tagName === 'example')
           .map((tag) => tag.text.trimEnd())
           .join('')
           .trim() ?? '';
+      examples = examples.replace(
+        /faker([A-Z]{2})\./g,
+        (_, locale: string) => `faker.locale = '${locales[locale]}';\nfaker.`
+      );
 
       expect(examples, `${moduleName}.${methodName} to have examples`).not.toBe(
         ''
       );
 
-      const examplesFixed = examples.replace(
-        /faker([A-Z]{2})\./g,
-        (_, locale: string) => `faker.locale = '${locales[locale]}';\nfaker.`
-      );
-
+      // Save examples to a file to run it
       const dir = resolve(__dirname, 'temp', moduleName);
       mkdirSync(dir, { recursive: true });
       const path = resolve(dir, `${methodName}.ts`);
       writeFileSync(
         path,
-        `import { faker } from '../../../../../src';\n${examplesFixed}`
+        `import { faker } from '../../../../../src';\n${examples}`
       );
 
+      // Run the examples
       await import(path);
 
+      // Verify logging
       const deprecatedFlag = signature.comment?.hasTag('deprecated') ?? false;
       if (deprecatedFlag) {
         expect(consoleSpies[1]).toHaveBeenCalled();
