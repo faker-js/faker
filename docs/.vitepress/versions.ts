c214ea66ef604bc268578ec59a4036d967b35709
Shinigami92 <chrissi92@hotmail.de>
2022-05-23 21:14:04 +0200

    chore: link to older docs site

---
+++ docs/.vitepress/versions.ts
@@ -0,0 +1,7 @@
+import { version } from '../../package.json';
+
+export const currentVersion = `v${version}`;
+
+export const versions = [
+  { version: 'v6.3.1', link: 'https://v6.fakerjs.dev/' },
+];
