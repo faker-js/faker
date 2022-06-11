94310078b3695325570efe5ff55711d5242d0d27
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-11 18:27:53 +0200

    refactor(address.nearbyGPSCoordinate): return type

--- src/modules/address/index.ts
+++ src/modules/address/index.ts
@@ -517,10 +517,10 @@ export class AddressModule {
     coordinate?: [latitude: number, longitude: number],
     radius: number = 10,
     isMetric: boolean = false
-  ): [latitude: string, longitude: string] {
+  ): [latitude: number, longitude: number] {
     // If there is no coordinate, the best we can do is return a random GPS coordinate.
     if (coordinate === undefined) {
-      return [this.latitude(), this.longitude()];
+      return [parseFloat(this.latitude()), parseFloat(this.longitude())];
     }
 
     const angleRadians = this.faker.datatype.float({
@@ -560,7 +560,7 @@ export class AddressModule {
     // Box longitude [-180°, 180°]
     newCoordinate[1] = (((newCoordinate[1] % 360) + 540) % 360) - 180;
 
-    return [newCoordinate[0].toFixed(4), newCoordinate[1].toFixed(4)];
+    return [newCoordinate[0], newCoordinate[1]];
   }
 
   /**
