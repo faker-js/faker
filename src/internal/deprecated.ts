41af10f5ea946b269555a9e8b577914cb5de7b96
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-13 18:53:17 +0200

    chore(deprecated): stricten options type

--- src/internal/deprecated.ts
+++ src/internal/deprecated.ts
@@ -1,24 +1,34 @@
 /* eslint-disable jsdoc/check-tag-names */
 /* eslint-disable jsdoc/require-param */
 
+type SemVer = `${number}.${number}.${number}`;
+
 /** @internal */
 export interface DeprecatedOptions {
   deprecated: string;
   proposed?: string;
-  since?: string;
-  until?: string;
+  since?: SemVer;
+  until?: SemVer;
 }
 
-/** @internal */
+/**
+ * @internal
+ *
+ * @param opts An options object.
+ * @param opts.deprecated The name of the function function following the syntax `faker.[module].[function]()`.
+ * @param opts.proposed An alternativ solution.
+ * @param opts.since The semver since when this is deprecated.
+ * @param opts.until The semver when this will be removed.
+ */
 export function deprecated(opts: DeprecatedOptions): void {
   let message = `[@faker-js/faker]: ${opts.deprecated} is deprecated`;
 
   if (opts.since) {
-    message += ` since ${opts.since}`;
+    message += ` since v${opts.since}`;
   }
 
   if (opts.until) {
-    message += ` and will be removed in ${opts.until}`;
+    message += ` and will be removed in v${opts.until}`;
   }
 
   if (opts.proposed) {
