0ef89f4019a40a5e58444c2751b33c20353df100
ST-DDT <ST-DDT@gmx.de>
2022-06-19 20:44:13 +0200

    test: convert company to test factory

--- test/company.spec.ts
+++ test/company.spec.ts
@@ -1,41 +1,31 @@
 import { afterEach, describe, expect, it } from 'vitest';
 import { faker } from '../src';
-import { seededRuns } from './support/seededRuns';
+import { seededTests } from './support/seededRuns';
 
 const NON_SEEDED_BASED_RUN = 5;
-
-const functionNames = [
-  'suffixes',
-  'companyName',
-  'companySuffix',
-  'catchPhrase',
-  'bs',
-  'catchPhraseAdjective',
-  'catchPhraseDescriptor',
-  'catchPhraseNoun',
-  'bsAdjective',
-  'bsBuzz',
-  'bsNoun',
-];
 
 describe('company', () => {
   afterEach(() => {
     faker.locale = 'en';
   });
 
-  for (const seed of seededRuns) {
-    describe(`seed: ${seed}`, () => {
-      for (const functionName of functionNames) {
-        it(`${functionName}()`, () => {
-          faker.seed(seed);
-
-          const actual = faker.company[functionName]();
-
-          expect(actual).toMatchSnapshot();
-        });
-      }
+  seededTests(faker, 'company', (t) => {
+    t.itEach(
+      'suffixes',
+      'companySuffix',
+      'catchPhrase',
+      'bs',
+      'catchPhraseAdjective',
+      'catchPhraseDescriptor',
+      'catchPhraseNoun',
+      'bsAdjective',
+      'bsBuzz',
+      'bsNoun'
+    );
+    t.describe('companyName', (t) => {
+      t.it('noArgs').it('with index');
     });
-  }
+  });
 
   describe(`random seeded tests for seed ${JSON.stringify(
     faker.seed()
