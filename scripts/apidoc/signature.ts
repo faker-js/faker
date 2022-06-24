2482e163805ce418d0df612d69aa6cebbe08c711
Shinigami92 <chrissi92@hotmail.de>
2022-06-24 13:34:05 +0200

    chore: skip sanitize html for now

--- scripts/apidoc/signature.ts
+++ scripts/apidoc/signature.ts
@@ -1,4 +1,4 @@
-import sanitizeHtml from 'sanitize-html';
+// import sanitizeHtml from 'sanitize-html';
 import type {
   Comment,
   DeclarationReflection,
@@ -62,16 +62,18 @@ const htmlSanitizeOptions: sanitizeHtml.IOptions = {
 
 function mdToHtml(md: string): string {
   const rawHtml = markdown.render(md);
-  const safeHtml: string = sanitizeHtml(rawHtml, htmlSanitizeOptions);
-  // Revert some escaped characters for comparison.
-  if (rawHtml.replace(/&gt;/g, '>') === safeHtml.replace(/&gt;/g, '>')) {
-    return safeHtml;
-  } else {
-    console.debug('Rejected unsafe md:', md);
-    console.error('Rejected unsafe html:', rawHtml.replace(/&gt;/g, '>'));
-    console.error('Expected safe html:', safeHtml.replace(/&gt;/g, '>'));
-    throw new Error('Found unsafe html');
-  }
+  // TODO @Shinigami92 2022-06-24: Sanitize html to prevent XSS
+  return rawHtml;
+  // const safeHtml: string = sanitizeHtml(rawHtml, htmlSanitizeOptions);
+  // // Revert some escaped characters for comparison.
+  // if (rawHtml.replace(/&gt;/g, '>') === safeHtml.replace(/&gt;/g, '>')) {
+  //   return safeHtml;
+  // } else {
+  //   console.debug('Rejected unsafe md:', md);
+  //   console.error('Rejected unsafe html:', rawHtml.replace(/&gt;/g, '>'));
+  //   console.error('Expected safe html:', safeHtml.replace(/&gt;/g, '>'));
+  //   throw new Error('Found unsafe html');
+  // }
 }
 
 export function analyzeSignature(
