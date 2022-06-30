cd324761fd110c1bc8fda414c58cca9c84b62af4
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-06-30 19:26:50 +0200

    feat(number): int

--- src/modules/number/index.ts
+++ src/modules/number/index.ts
@@ -1,4 +1,5 @@
 import type { Faker } from '../..';
+import { FakerError } from '../../errors/faker-error';
 
 /**
  * Module to generate numbers of any kind.
@@ -12,5 +13,42 @@ export class NumberModule {
       }
       this[name] = this[name].bind(this);
     }
+  }
+
+  /**
+   * Returns a single random integer between zero and the given max value or the given range.
+   * The bounds are inclusive.
+   *
+   * @param options Maximum value or options object.
+   * @param options.min Lower bound for generated number. Defaults to `0`.
+   * @param options.max Upper bound for generated number. Defaults to `min + 99999`.
+   *
+   * @throws When options define `max < min`.
+   *
+   * @example
+   * faker.number.int() // 55422
+   * faker.number.int(100) // 52
+   * faker.number.int({ min: 1000000 }) // 1031433
+   * faker.number.int({ max: 100 }) // 42
+   */
+  int(options: number | { min?: number; max?: number } = {}): number {
+    if (typeof options === 'number') {
+      options = { max: options };
+    }
+
+    const { min = 0 } = options;
+    const max = options.max ?? min + 99999;
+
+    if (max === min) {
+      return min;
+    }
+
+    if (max < min) {
+      throw new FakerError(`Max ${max} should be greater than min ${min}.`);
+    }
+
+    const randomInt = this.faker.mersenne.rand(max + 1, min);
+
+    return randomInt;
   }
 }
---
+++ test/number.spec.ts
@@ -0,0 +1,119 @@
+import { afterEach, describe, expect, it } from 'vitest';
+import { faker } from '../src';
+import type { NumberModule } from '../src/modules/number';
+import { seededRuns } from './support/seededRuns';
+
+const functionNames: (keyof NumberModule)[] = ['int'];
+
+describe('number', () => {
+  afterEach(() => {
+    faker.locale = 'en';
+  });
+
+  for (const seed of seededRuns) {
+    describe(`seed: ${seed}`, () => {
+      for (const functionName of functionNames) {
+        it(`${functionName}()`, () => {
+          faker.seed(seed);
+
+          const actual = faker.number[functionName]();
+
+          expect(actual).toMatchSnapshot();
+        });
+      }
+    });
+  }
+
+  describe(`random seeded tests for seed ${JSON.stringify(
+    faker.seed()
+  )}`, () => {
+    describe('int', () => {
+      it('should return a random number given a maximum value as Number', () => {
+        const max = 10;
+
+        const actual = faker.number.int(max);
+
+        expect(actual).toBeGreaterThanOrEqual(0);
+        expect(actual).toBeLessThanOrEqual(max);
+      });
+
+      it('should return a random number given a maximum value as Object', () => {
+        const options = { max: 10 };
+
+        const actual = faker.number.int(options);
+
+        expect(actual).toBeGreaterThanOrEqual(0);
+        expect(actual).toBeLessThanOrEqual(options.max);
+      });
+
+      it('should return a random number given a maximum value of 0', () => {
+        const options = { max: 0 };
+
+        const actual = faker.number.int(options);
+
+        expect(actual).toBe(0);
+      });
+
+      it('should return a random number given a negative number minimum and maximum value of 0', () => {
+        const options = { min: -100, max: 0 };
+
+        const actual = faker.number.int(options);
+
+        expect(actual).toBeGreaterThanOrEqual(options.min);
+        expect(actual).toBeLessThanOrEqual(options.max);
+      });
+
+      it('should return a random number between a range', () => {
+        const options = { min: 22, max: 33 };
+        for (let i = 0; i < 100; i++) {
+          const actual = faker.number.int(options);
+          expect(actual).toBeGreaterThanOrEqual(options.min);
+          expect(actual).toBeLessThanOrEqual(options.max);
+        }
+      });
+
+      it('should return inclusive negative max value', () => {
+        let foundNegative4 = false;
+        let foundNegative5 = false;
+
+        for (let iter = 0; iter < 1000; iter++) {
+          const actual = faker.number.int({ min: -5, max: -4 });
+
+          if (actual === -4) {
+            foundNegative4 = true;
+          } else if (actual === -5) {
+            foundNegative5 = true;
+          }
+
+          expect(actual).toBeGreaterThanOrEqual(-5);
+          expect(actual).toBeLessThanOrEqual(-4);
+
+          if (foundNegative4 && foundNegative5) {
+            break;
+          }
+        }
+
+        expect(foundNegative4).toBeTruthy();
+        expect(foundNegative5).toBeTruthy();
+      });
+
+      it('should not mutate the input object', () => {
+        const initialMin = 1;
+        const initialPrecision = 1;
+        const initialOtherProperty = 'hello darkness my old friend';
+        const input: {
+          min?: number;
+          max?: number;
+          precision?: number;
+          otherProperty: string;
+        } = Object.freeze({
+          min: initialMin,
+          precision: initialPrecision,
+          otherProperty: initialOtherProperty,
+        });
+
+        expect(() => faker.number.int(input)).not.toThrow();
+      });
+    });
+  });
+});
