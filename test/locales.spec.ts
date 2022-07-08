86b2631c8340a324961f393e2b878015bd4ecaa5
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-07-09 00:58:01 +0200

    test(locales): check for duplicated entries

--- test/locales.spec.ts
+++ test/locales.spec.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'vitest';
+import type { LocaleDefinition } from '../src';
 import { faker } from '../src';
+import allLocales from '../src/locales';
 
 // Remark: actual use of locales functionality is currently tested in all.functional.js test
 
@@ -12,4 +14,45 @@ describe('locale', () => {
       }
     });
   });
+
+  for (const [localeName, moduleMap] of Object.entries(allLocales)) {
+    describe(localeName, () => {
+      for (const [
+        moduleName,
+        definitionMap,
+      ] of Object.entries<LocaleDefinition>(moduleMap)) {
+        if (moduleName === 'title' || moduleName === 'separator') {
+          continue;
+        }
+
+        describe(moduleName, () => {
+          for (const [definitionName, entries] of Object.entries(
+            definitionMap
+          )) {
+            describe(definitionName, () => {
+              if (Array.isArray(entries)) {
+                it('should have no duplicate entries', () => {
+                  const uniques = new Set(entries);
+                  const duplications = entries.filter((entry) => {
+                    if (uniques.has(entry)) {
+                      uniques.delete(entry);
+                      return false;
+                    } else {
+                      return true;
+                    }
+                  });
+                  const uniqueDuplication = [...new Set(duplications)];
+
+                  expect(
+                    uniqueDuplication,
+                    `Duplicated values are: [${uniqueDuplication.join(', ')}]`
+                  ).toHaveLength(0);
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
 });
