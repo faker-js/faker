63c3ff46246fa8e884476c1478549a1c5d8ed455
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-07-14 15:00:51 +0200

    test(StringModule): alpha

--- test/string.spec.ts
+++ test/string.spec.ts
@@ -1,11 +1,16 @@
 import { afterEach, describe, expect, it } from 'vitest';
-import { faker } from '../src';
+import { faker, FakerError } from '../src';
 import type { StringModule } from '../src/modules/string';
 import { seededRuns } from './support/seededRuns';
 
 const NON_SEEDED_BASED_RUN = 5;
 
-const functionNames: (keyof StringModule)[] = ['uuid', 'hexadecimal', 'random'];
+const functionNames: (keyof StringModule)[] = [
+  'uuid',
+  'hexadecimal',
+  'random',
+  'alpha',
+];
 
 describe('string', () => {
   afterEach(() => {
@@ -98,6 +103,107 @@ describe('string', () => {
           const hex = faker.string.hexadecimal(5);
           expect(hex).toMatch(/^[0-9a-f]*$/i);
           expect(hex).toHaveLength(5);
+        });
+      });
+
+      describe('alpha()', () => {
+        it('should return single letter when no count provided', () => {
+          const actual = faker.string.alpha();
+
+          expect(actual).toHaveLength(1);
+        });
+
+        it('should return any letters when no option is provided', () => {
+          const actual = faker.string.alpha();
+
+          expect(actual).toMatch(/^[a-zA-Z]$/);
+        });
+
+        it.each([
+          ['upper', /^[A-Z]{250}$/],
+          ['lower', /^[a-z]{250}$/],
+          ['mixed', /^[a-zA-Z]{250}$/],
+        ] as const)('should return %s-case', (casing, pattern) => {
+          const actual = faker.string.alpha({ count: 250, casing });
+          expect(actual).toMatch(pattern);
+        });
+
+        it('should generate many random letters', () => {
+          const actual = faker.string.alpha(5);
+
+          expect(actual).toHaveLength(5);
+        });
+
+        it.each([0, -1, -100])(
+          'should return empty string when length is <= 0',
+          (length) => {
+            const actual = faker.string.alpha(length);
+
+            expect(actual).toBe('');
+          }
+        );
+
+        it('should be able to ban some characters', () => {
+          const actual = faker.string.alpha({
+            count: 5,
+            bannedChars: ['a', 'p'],
+            casing: 'lower',
+          });
+
+          expect(actual).toHaveLength(5);
+          expect(actual).toMatch(/^[b-oq-z]{5}$/);
+        });
+
+        it('should be able to ban some characters via string', () => {
+          const actual = faker.string.alpha({
+            count: 5,
+            bannedChars: 'ap',
+            casing: 'lower',
+          });
+
+          expect(actual).toHaveLength(5);
+          expect(actual).toMatch(/^[b-oq-z]{5}$/);
+        });
+
+        it('should be able handle mistake in banned characters array', () => {
+          const alphaText = faker.string.alpha({
+            count: 5,
+            bannedChars: ['a', 'a', 'p'],
+            casing: 'lower',
+          });
+
+          expect(alphaText).toHaveLength(5);
+          expect(alphaText).toMatch(/^[b-oq-z]{5}$/);
+        });
+
+        it('should throw if all possible characters being banned', () => {
+          const bannedChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+          expect(() =>
+            faker.string.alpha({
+              count: 5,
+              bannedChars,
+              casing: 'lower',
+            })
+          ).toThrowError(
+            new FakerError(
+              'Unable to generate string, because all possible characters are banned.'
+            )
+          );
+        });
+
+        it('should not mutate the input object', () => {
+          const input: {
+            count: number;
+            casing: 'mixed';
+            bannedChars: string[];
+          } = Object.freeze({
+            count: 5,
+            casing: 'mixed',
+            bannedChars: ['a', '%'],
+          });
+
+          expect(() => faker.string.alpha(input)).not.toThrow();
+          expect(input.bannedChars).toEqual(['a', '%']);
         });
       });
     }
