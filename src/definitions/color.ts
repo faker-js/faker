0b2e5f5fc8155c6d3e5584714018489ae48032b1
Hanna Kuczynska <hania.kuczynska@gmail.com>
2022-07-20 13:33:03 +0200

    docs(definitions): minor formatting to definitions' types

--- src/definitions/color.ts
+++ src/definitions/color.ts
@@ -1,13 +1,14 @@
 import type { LocaleEntry } from './definitions';
 
 /**
- * The possible definitions related to color.
+ * The possible definitions related to colors.
  */
 export type ColorDefinitions = LocaleEntry<{
   /**
-   * Human readable color names
+   * Human readable color names.
    */
   human: string[];
+
   /**
    * Color space names.
    */
--- src/definitions/hacker.ts
+++ src/definitions/hacker.ts
@@ -8,26 +8,31 @@ export type HackerDefinitions = LocaleEntry<{
    * Generic computer related abbreviations (e.g. `RAM`, `EXE`).
    */
   abbreviation: string[];
+
   /**
    * Some computer related adjectives or descriptors (e.g. `digital`, `bluetooth`)
    */
   adjective: string[];
+
   /**
    * Some computer related verbs for continuous actions (en: `ing` suffix; e.g. `hacking`).
    */
   ingverb: string[];
+
   /**
-   * Some computer related nouns (e.g. `protocol`, `sensor`)
+   * Some computer related nouns (e.g. `protocol`, `sensor`).
    */
   noun: string[];
+
   /**
    * Some phrases that will be injected with random hacker words.
    * May use any of the HackerDefinition keys wrapped in double braces.
-   * (e.g. `I'm {{ingverb}} {{adjective}} {{noun}}` )
+   * (e.g. `I'm {{ingverb}} {{adjective}} {{noun}}` ).
    *
    * @see Helpers.mustache()
    */
   phrase: string[];
+
   /**
    * Some computer related verbs (e.g. `hack`).
    */
