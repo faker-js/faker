c2343d0fae63c232206c44b650c83595ad49f8be
Hanna Kuczynska <hania.kuczynska@gmail.com>
2022-08-03 21:07:32 +0200

    feat(locales.pl): add pl translation for directions and directionss abbr

---
+++ src/locales/pl/address/direction.ts
@@ -0,0 +1,10 @@
+export default [
+  'Północ',
+  'Południe',
+  'Wschód',
+  'Zachód',
+  'Północny Wschód',
+  'Północny Zachód',
+  'Południowy Wschód',
+  'Południowy Zachód',
+];
---
+++ src/locales/pl/address/direction_abbr.ts
@@ -0,0 +1,1 @@
+export default ['N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW'];
