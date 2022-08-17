263da5cf27f9e008dda88411692a240225203d38
Luke Watts <luke@thisis.la>
2022-08-17 07:59:29 +0200

    chore: update JSdoc based on review feedback

--- src/internal/toDate.ts
+++ src/internal/toDate.ts
@@ -2,7 +2,7 @@
  * Converts date passed as a string, number or Date to a Date object.
  * If nothing or a non parseable value is passed, takes current date.
  *
- * @param date Date
+ * @param date The input to convert to a date.
  */
 export function toDate(date?: string | Date | number): Date {
   date = new Date(date);
