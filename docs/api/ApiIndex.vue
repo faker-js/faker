87f63084b40c3d240ed3403787117886321e9c0f
Shinigami92 <chrissi92@hotmail.de>
2022-08-20 12:58:50 +0200

    chore: improve comment

--- docs/api/ApiIndex.vue
+++ docs/api/ApiIndex.vue
@@ -51,7 +51,7 @@ function slugify(text: string): string {
       .replace(/\-{2,}/g, '-')
       // Remove prefixing and trailing separators
       .replace(/^\-+|\-+$/g, '')
-      // ensure it doesn't start with a number (#121)
+      // ensure it doesn't start with a number (like #123)
       .replace(/^(\d)/, '_$1')
     // // lowercase
     // .toLowerCase()
