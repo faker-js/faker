f1e361c6b09b47282b73e2a5fdc907f6715f1ee9
Shinigami92 <chrissi92@hotmail.de>
2022-08-20 13:03:32 +0200

    docs: add JSDoc

--- docs/api/api-types.ts
+++ docs/api/api-types.ts
@@ -1,14 +1,23 @@
+/**
+ * Represents a link to e.g. a Faker-Module method within the API search index page.
+ */
 export interface APIHeader {
   anchor: string;
   text: string;
 }
 
+/**
+ * Represents a container for e.g. a Faker-Module within the API search index page.
+ */
 export interface APIItem {
   text: string;
   link: string;
   headers: APIHeader[];
 }
 
+/**
+ * Represents a whole section within the API search index page.
+ */
 export interface APIGroup {
   text: string;
   items: APIItem[];
