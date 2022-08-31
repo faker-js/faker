a461846398c41c6becf31b1342c8ab1b14cad0b3
Pablo León Manzano <p.leon@dipualba.es>
2022-08-31 09:58:42 +0200

    feat(locale): new date dataset for spanish

---
+++ src/locales/es/date/weekday.ts
@@ -0,0 +1,27 @@
+// Sources: https://www.unicode.org/cldr/cldr-aux/charts/28/summary/es.html and https://www.wikilengua.org/index.php/Abreviaciones_en_fechas
+export default {
+  wide: [
+    'domingo',
+    'lunes',
+    'martes',
+    'miércoles',
+    'jueves',
+    'viernes',
+    'sábado',
+  ],
+  // Property "wide_context" is optional, if not set then "wide" will be used instead
+  // It is used to specify a word in context, which may differ from a stand-alone word
+  wide_context: [
+    'domingo',
+    'lunes',
+    'martes',
+    'miércoles',
+    'jueves',
+    'viernes',
+    'sábado',
+  ],
+  abbr: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+  // Property "abbr_context" is optional, if not set then "abbr" will be used instead
+  // It is used to specify a word in context, which may differ from a stand-alone word
+  abbr_context: ['dom.', 'lun.', 'mart.', 'miérc.', 'juev.', 'vier.', 'sáb.'],
+};
