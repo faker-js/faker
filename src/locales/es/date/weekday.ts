0ea89c2566eddd27dc0e300a899626ba1482aeb7
P. León <muchasmegas@gmail.com>
2022-08-31 13:40:40 +0200

    Update weekday.ts
    Removing wide_context as requested

--- src/locales/es/date/weekday.ts
+++ src/locales/es/date/weekday.ts
@@ -9,17 +9,6 @@ export default {
     'viernes',
     'sábado',
   ],
-  // Property "wide_context" is optional, if not set then "wide" will be used instead
-  // It is used to specify a word in context, which may differ from a stand-alone word
-  wide_context: [
-    'domingo',
-    'lunes',
-    'martes',
-    'miércoles',
-    'jueves',
-    'viernes',
-    'sábado',
-  ],
   abbr: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
   // Property "abbr_context" is optional, if not set then "abbr" will be used instead
   // It is used to specify a word in context, which may differ from a stand-alone word
