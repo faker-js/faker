4ab4acfd30e4be4fa8ea2eb0020e83f9e1f53f5f
Shinigami92 <chrissi92@hotmail.de>
2022-09-29 16:59:01 +0200

    chore: format

--- src/locales/zh_CN/name/index.ts
+++ src/locales/zh_CN/name/index.ts
@@ -5,8 +5,8 @@
 import type { NameDefinitions } from '../../..';
 import female_first_name from './female_first_name';
 import first_name from './first_name';
-import male_first_name from './male_first_name';
 import last_name from './last_name';
+import male_first_name from './male_first_name';
 import name_ from './name';
 
 const name: NameDefinitions = {
