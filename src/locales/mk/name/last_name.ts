3c31ef7a17f15fbf360287e59f057b6b42a59dd5
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-09-29 19:15:25 +0200

    refactor(locale): unique name combinations

--- src/locales/mk/name/last_name.ts
+++ src/locales/mk/name/last_name.ts
@@ -1,4 +1,4 @@
 import female_last_name from './female_last_name';
 import male_last_name from './male_last_name';
 
-export default female_last_name.concat(male_last_name);
+export default [...new Set([...female_last_name, ...male_last_name])];
