4bc31d4a8c1a6c5a4d41a9be77e57142b51c9a24
ST-DDT <ST-DDT@gmx.de>
2022-10-13 11:23:38 +0200

    docs: show next version as special one

--- docs/.vitepress/versions.ts
+++ docs/.vitepress/versions.ts
@@ -1,7 +1,12 @@
 import { version } from '../../package.json';
 
-export const currentVersion = `v${version}`;
+const isNext = true;
+
+export const currentVersion = isNext ? 'Next' : `v${version}`;
+
+const latestVersion = { version: `v${version}`, link: 'https://fakerjs.dev/' };
 
 export const oldVersions = [
+  ...(isNext ? [latestVersion] : []),
   { version: 'v6.3.1', link: 'https://v6.fakerjs.dev/' },
 ];
