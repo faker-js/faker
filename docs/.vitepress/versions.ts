3c0b3b4aeac26ee6401f9eb5d447868a845ed331
ST-DDT <ST-DDT@gmx.de>
2022-10-16 12:18:23 +0200

    docs: automatically generate links to other versions

--- docs/.vitepress/versions.ts
+++ docs/.vitepress/versions.ts
@@ -3,7 +3,31 @@ import { version } from '../../package.json';
 
 const NEXT_TEXT = 'Next';
 
-function readBranchName() {
+export function extractMajorVersionNumber(version: string): number {
+  if (version.startsWith('v')) {
+    return Number(version.split('.')[0].substring(1));
+  } else {
+    return NaN;
+  }
+}
+
+export function pickLatest(version1: string, version2: string): string {
+  const parts1 = version1.split('-', 2);
+  const parts2 = version2.split('-', 2);
+  let compare = parts1[0].localeCompare(parts2[0]);
+  if (compare === 0) {
+    if (parts1.length === 1) {
+      compare = 1;
+    } else if (parts2.length === 1) {
+      compare = -1;
+    } else {
+      compare = parts1[1].localeCompare(parts2[1]);
+    }
+  }
+  return compare > 0 ? version1 : version2;
+}
+
+function readBranchName(): string {
   try {
     return execSync('git branch --show-current').toString('utf8') || NEXT_TEXT;
   } catch {
@@ -11,15 +35,54 @@ function readBranchName() {
   }
 }
 
-const branchName = readBranchName();
-const isNext = !branchName.match(/^v\d+$/);
+function readOtherLatestReleaseTagNames(): string[] {
+  const currentMajorVersion = extractMajorVersionNumber(`v${version}`);
+  try {
+    const latestReleaseTagNames = execSync('git tag -l')
+      .toString('utf8')
+      .split('\n')
+      .reduce<Record<string, string>>((acc, tag) => {
+        const majorVersion = extractMajorVersionNumber(tag);
+        if (
+          // Only consider tags that are version tags
+          !isNaN(majorVersion) &&
+          // and that are created after we took the project over
+          majorVersion >= 6 &&
+          // and that are not the current version (because we are already on that branch)
+          // also ignore later version, to avoid changing the deployed version list
+          majorVersion < currentMajorVersion
+        ) {
+          if (/\d{2,}/.test(tag)) {
+            // if this happens, we have to update the version compare logic
+            throw new Error(`Unsupported tag name: ${tag}`);
+          }
+          acc[`v${majorVersion}`] = pickLatest(acc[majorVersion] || '', tag);
+        }
+        return acc;
+      }, {});
 
-export const currentVersion = isNext ? NEXT_TEXT : `v${version}`;
+    return Object.values(latestReleaseTagNames).sort(
+      (a, b) => extractMajorVersionNumber(b) - extractMajorVersionNumber(a)
+    );
+  } catch {
+    return [];
+  }
+}
+
+const branchName = readBranchName();
+const isNext = !/^v\d+$/.test(branchName);
 
 const nextVersion = { version: NEXT_TEXT, link: 'https://next.fakerjs.dev/' };
 const latestVersion = { version: `v${version}`, link: 'https://fakerjs.dev/' };
+const otherVersions = readOtherLatestReleaseTagNames();
 
+export const currentVersion = isNext ? NEXT_TEXT : `v${version}`;
 export const oldVersions = [
   isNext ? latestVersion : nextVersion,
-  { version: 'v6.3.1', link: 'https://v6.fakerjs.dev/' },
+  ...otherVersions.map((version) => ({
+    version,
+    link: `https://v${extractMajorVersionNumber(version)}.fakerjs.dev/`,
+  })),
 ];
+
+console.log(oldVersions);
--- test/docs/versions.spec.ts
+++ test/docs/versions.spec.ts
@@ -1,42 +1,94 @@
 import { describe, expect, it } from 'vitest';
-import { currentVersion, oldVersions } from '../../docs/.vitepress/versions';
+import {
+  currentVersion,
+  extractMajorVersionNumber,
+  oldVersions,
+  pickLatest,
+} from '../../docs/.vitepress/versions';
 import { version } from '../../package.json';
 
-function extractMajorVersionNumber(version: string): number {
-  return Number(version.split('.')[0].substring(1));
-}
-
 describe('docs versions', () => {
-  it('should have a complete set of oldVersions', () => {
-    const versionText = `v${version}`;
+  describe('extractMajorVersionNumber', () => {
+    it('should extract major version number', () => {
+      expect(extractMajorVersionNumber('v6.0.0')).toBe(6);
+      expect(extractMajorVersionNumber('v6.0.0-beta.1')).toBe(6);
+      expect(extractMajorVersionNumber('V6.0.0')).toBeNaN();
+      expect(extractMajorVersionNumber('Very.Important.Tag')).toBeNaN();
+    });
+  });
 
-    expect(oldVersions.length).toBeGreaterThanOrEqual(1);
-    const currentMajorVersion = extractMajorVersionNumber(versionText);
+  describe('pickLatest', () => {
+    it('should pick the latest version', () => {
+      // Same versions
+      expect(pickLatest('v6.0.0', 'v6.0.0')).toBe('v6.0.0');
+      expect(pickLatest('v6.0.0-alpha.0', 'v6.0.0-alpha.0')).toBe(
+        'v6.0.0-alpha.0'
+      );
+      // Major versions
+      expect(pickLatest('v6.0.0', 'v7.0.0')).toBe('v7.0.0');
+      expect(pickLatest('v7.0.0', 'v6.0.0')).toBe('v7.0.0');
+      // Minor versions
+      expect(pickLatest('v6.0.0', 'v6.1.0')).toBe('v6.1.0');
+      expect(pickLatest('v6.1.0', 'v6.0.0')).toBe('v6.1.0');
+      // Patch versions
+      expect(pickLatest('v6.0.0', 'v6.0.1')).toBe('v6.0.1');
+      expect(pickLatest('v6.0.1', 'v6.0.0')).toBe('v6.0.1');
+      // Tag
+      expect(pickLatest('v6.0.0', 'v6.0.0-alpha.0')).toBe('v6.0.0');
+      expect(pickLatest('v6.0.0-alpha.0', 'v6.0.0')).toBe('v6.0.0');
+      expect(pickLatest('v6.0.0-alpha.0', 'v6.0.0-alpha.1')).toBe(
+        'v6.0.0-alpha.1'
+      );
+      expect(pickLatest('v6.0.0-alpha.1', 'v6.0.0-alpha.0')).toBe(
+        'v6.0.0-alpha.1'
+      );
+      expect(pickLatest('v6.0.0-alpha.0', 'v6.0.0-beta.0')).toBe(
+        'v6.0.0-beta.0'
+      );
+      expect(pickLatest('v6.0.0-beta.0', 'v6.0.0-alpha.0')).toBe(
+        'v6.0.0-beta.0'
+      );
+      // Vastly different versions
+      expect(pickLatest('v7.0.0', 'v6.0.0-alpha.1')).toBe('v7.0.0');
+      expect(pickLatest('v7.0.0-alpha.1', 'v6.0.0')).toBe('v7.0.0-alpha.1');
+      expect(pickLatest('v7.0.0-alpha.1', 'v6.0.0-beta.3')).toBe(
+        'v7.0.0-alpha.1'
+      );
+    });
+  });
 
-    if (currentVersion === 'Next') {
-      expect(oldVersions[0]).toEqual({
-        version: versionText,
-        link: 'https://fakerjs.dev/',
-      });
+  describe('oldVersions', () => {
+    it('should have a complete set of oldVersions', () => {
+      const versionText = `v${version}`;
 
-      for (let i = 0; i < oldVersions.length; i++) {
-        const oldMajorVersion = extractMajorVersionNumber(
-          oldVersions[i].version
-        );
-        expect(oldMajorVersion).toBe(currentMajorVersion - i);
+      expect(oldVersions.length).toBeGreaterThanOrEqual(2);
+      const currentMajorVersion = extractMajorVersionNumber(versionText);
+
+      if (currentVersion === 'Next') {
+        expect(oldVersions[0]).toEqual({
+          version: versionText,
+          link: 'https://fakerjs.dev/',
+        });
+
+        for (let i = 0; i < oldVersions.length; i++) {
+          const oldMajorVersion = extractMajorVersionNumber(
+            oldVersions[i].version
+          );
+          expect(oldMajorVersion).toBe(currentMajorVersion - i);
+        }
+      } else {
+        expect(oldVersions[0]).toEqual({
+          version: 'Next',
+          link: 'https://next.fakerjs.dev/',
+        });
+
+        for (let i = 1; i < oldVersions.length; i++) {
+          const entry = oldVersions[i];
+          const oldMajorVersion = extractMajorVersionNumber(entry.version);
+          expect(oldMajorVersion).toBe(currentMajorVersion - i);
+          expect(entry.link).toMatch(/https:\/\/v\d+\.fakerjs\.dev/);
+        }
       }
-    } else {
-      expect(oldVersions[0]).toEqual({
-        version: 'Next',
-        link: 'https://next.fakerjs.dev/',
-      });
-
-      for (let i = 1; i < oldVersions.length; i++) {
-        const oldMajorVersion = extractMajorVersionNumber(
-          oldVersions[i].version
-        );
-        expect(oldMajorVersion).toBe(currentMajorVersion - i);
-      }
-    }
+    });
   });
 });
