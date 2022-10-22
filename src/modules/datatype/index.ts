5ab7c0810a61ad303462c3eb0ba5e234a0f96683
ST-DDT <ST-DDT@gmx.de>
2022-10-22 14:07:09 +0200

    chore: cleanup

--- src/modules/datatype/index.ts
+++ src/modules/datatype/index.ts
@@ -216,7 +216,7 @@ export class DatatypeModule {
       // This check is required to avoid returning false when float() returns 1
       return true;
     }
-    return this.faker.datatype.float({ min: 0, max: 1 }) < probability;
+    return this.float({ min: 0, max: 1 }) < probability;
   }
 
   /**
