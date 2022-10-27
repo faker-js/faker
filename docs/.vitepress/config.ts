315c24f2656fdc558637e48e1578969640a5ab71
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-10-27 20:55:50 +0530

    revert

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -75,6 +75,13 @@ const config = defineConfig({
     [
       'meta',
       {
+        name: 'description',
+        content: description,
+      },
+    ],
+    [
+      'meta',
+      {
         name: 'og:image',
         content: image,
       },
