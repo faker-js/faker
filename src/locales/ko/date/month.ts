055704bfa1eb686eded9702671be9405cf7aec20
thilllon <thilllon970@gmail.com>
2022-10-28 11:58:26 +0900

    Update month.ts

--- src/locales/ko/date/month.ts
+++ src/locales/ko/date/month.ts
@@ -15,20 +15,7 @@ export default {
   ],
   // Property "wide_context" is optional, if not set then "wide" will be used instead
   // It is used to specify a word in context, which may differ from a stand-alone word
-  wide_context: [
-    '1월',
-    '2월',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
+  // wide_context: [],
   abbr: [
     '1월',
     '2월',
@@ -45,18 +32,5 @@ export default {
   ],
   // Property "abbr_context" is optional, if not set then "abbr" will be used instead
   // It is used to specify a word in context, which may differ from a stand-alone word
-  abbr_context: [
-    '1월',
-    '2월',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
+  // abbr_context: [],
 };
