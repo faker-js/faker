88f147b233dac33e09b8a8a524e0365b68c14605
subbe <mohamed.suhaib@gmail.com>
2022-10-31 13:41:03 +0500

    feat(locale): add dv locale

---
+++ src/locales/dv/company/name_patterns.ts
@@ -0,0 +1,7 @@
+export default [
+  '{{person.first_name}} {{company.suffix}}',
+  '{{person.last_name}} {{company.suffix}}',
+  '{{person.last_name}} & {{person.last_name}} {{company.noun}} {{company.suffix}}',
+  '{{company.adjective}} {{company.noun}} {{company.suffix}}',
+  '{{company.adjective}} {{person.last_name}} {{person.last_name}} {{company.suffix}}',
+];
