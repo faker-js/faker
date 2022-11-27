201d4f8dd6ee3be366931b7574a9ff3b2e0b995c
Matt Mayer <matt@matthewmayer.co.uk>
2022-11-27 22:47:40 +0700

    fix(person): Incorrect behavior of name.firstName() function - split gendered first_name for nl_BE, en_IN, en_AU and de_AT

--- src/locales/de_AT/person/index.ts
+++ src/locales/de_AT/person/index.ts
@@ -3,15 +3,19 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { PersonDefinitions } from '../../..';
+import female_first_name from './female_first_name';
 import first_name from './first_name';
 import last_name from './last_name';
+import male_first_name from './male_first_name';
 import name_ from './name';
 import nobility_title_prefix from './nobility_title_prefix';
 import prefix from './prefix';
 
 const person: PersonDefinitions = {
+  female_first_name,
   first_name,
   last_name,
+  male_first_name,
   name: name_,
   nobility_title_prefix,
   prefix,
