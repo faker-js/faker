5eb386f6ea682cf44b3cd184def32cb8ed601087
Matt Mayer <matt@matthewmayer.co.uk>
2022-11-27 17:53:44 +0700

    fix(person): Incorrect behavior of name.firstName() function - remove duplicates

--- src/locales/sk/person/first_name.ts
+++ src/locales/sk/person/first_name.ts
@@ -1,6 +1,7 @@
 // We don't have a generic first name list for this locale
-// So simply concatenate male and female lists to avoid falling back to fallback locale
+// So simply concatenate male and female lists and remove any duplicates
+// This avoids falling back to fallback locale
 import female_first_name from './female_first_name';
 import male_first_name from './male_first_name';
 
-export default [...female_first_name, ...male_first_name];
+export default [...new Set([...female_first_name, ...male_first_name])];
