a6d77f00130168c6046a074b292a1069e1478fc2
Matt Mayer <matt@matthewmayer.co.uk>
2022-11-30 23:54:58 +0700

    fix(locale): improve en_GB location addresses

--- src/locales/en_GB/location/index.ts
+++ src/locales/en_GB/location/index.ts
@@ -3,19 +3,29 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { LocationDefinitions } from '../../..';
+import building_number from './building_number';
 import city from './city';
+import city_infix from './city_infix';
+import city_prefix from './city_prefix';
+import city_suffix from './city_suffix';
 import county from './county';
 import default_country from './default_country';
 import postcode from './postcode';
 import street from './street';
+import street_suffix from './street_suffix';
 import uk_country from './uk_country';
 
 const location: LocationDefinitions = {
+  building_number,
   city,
+  city_infix,
+  city_prefix,
+  city_suffix,
   county,
   default_country,
   postcode,
   street,
+  street_suffix,
   uk_country,
 };
 
