4ed71734f32714a76d004da83f05f6310b1716be
Matt Mayer <matt@matthewmayer.co.uk>
2022-12-18 09:27:38 +0700

    fix(person): use new mergeArrays function

--- src/locales/sk/person/first_name.ts
+++ src/locales/sk/person/first_name.ts
@@ -1,7 +1,8 @@
 // We don't have a generic first name list for this locale
 // So simply concatenate male and female lists and remove any duplicates
 // This avoids falling back to fallback locale
+import { mergeArrays } from './../../../internal/merge';
 import female_first_name from './female_first_name';
 import male_first_name from './male_first_name';
 
-export default [...new Set([...female_first_name, ...male_first_name])];
+export default mergeArrays(female_first_name, male_first_name);
