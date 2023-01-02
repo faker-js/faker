8ee18de105b13eb9e2768369f3c8fe48787b1f0e
ST-DDT <ST-DDT@gmx.de>
2023-01-02 01:36:47 +0100

    feat(locales): add mergeLocales utility

---
+++ src/utils/merge-locales.ts
@@ -0,0 +1,37 @@
+import type { LocaleDefinition } from 'src';
+
+/**
+ * Merges the given locales into one locale.
+ * The locales are merged in the order they are given.
+ * The first locale that provides a entry for a category will be used for that.
+ * Mutating the category entries in the returned locale will also mutate the entries in the respective source locale.
+ *
+ * @param locales The locales to merge.
+ * @returns The newly merged locale.
+ *
+ * @example
+ * const locale = mergeLocales([ de_CH, de, en ]);
+ */
+export function mergeLocales(locales: LocaleDefinition[]): LocaleDefinition {
+  const merged: LocaleDefinition = {} as LocaleDefinition;
+
+  for (const locale of locales) {
+    for (const key in locale) {
+      if (merged[key] === undefined) {
+        if (typeof locale[key] === 'object') {
+          merged[key] = { ...locale[key] };
+        } else {
+          merged[key] = locale[key];
+        }
+      } else {
+        if (typeof locale[key] === 'object') {
+          merged[key] = { ...locale[key], ...merged[key] };
+        } else {
+          // Do nothing
+        }
+      }
+    }
+  }
+
+  return merged;
+}
---
+++ test/utils/merge-locales.spec.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from 'vitest';
+import type { LocaleDefinition } from '../../src';
+import { mergeLocales } from '../../src/utils/merge-locales';
+
+describe('mergeLocales', () => {
+  it('should overwrite locales', () => {
+    const locale1: LocaleDefinition = {
+      title: 'a',
+      person: { firstName: ['a'] },
+    };
+    const locale2: LocaleDefinition = {
+      title: 'b',
+      person: { firstName: ['b'] },
+    };
+    const locale3: LocaleDefinition = {
+      title: 'c',
+      person: { firstName: ['c'] },
+    };
+
+    const merged = mergeLocales([locale1, locale2, locale3]);
+
+    expect(merged).toEqual({
+      title: 'a',
+      person: { firstName: ['a'] },
+    });
+  });
+
+  it('should extend locales', () => {
+    const locale1: LocaleDefinition = {
+      title: 'a',
+      location: { city: ['a'] },
+      person: { first_name: ['a'] },
+    };
+    const locale2: LocaleDefinition = {
+      title: 'b',
+      animal: { cat: ['b'] },
+      person: { last_name: ['b'] },
+    };
+    const locale3: LocaleDefinition = {
+      title: 'c',
+      color: { human: ['c'] },
+      person: {},
+    };
+
+    const merged = mergeLocales([locale1, locale2, locale3]);
+
+    expect(merged).toEqual({
+      title: 'a',
+      animal: { cat: ['b'] },
+      color: { human: ['c'] },
+      location: { city: ['a'] },
+      person: { first_name: ['a'], last_name: ['b'] },
+    });
+  });
+});
