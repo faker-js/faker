274df35995db9bc23d53910e2daa7e53893fe10b
Matt Mayer <matt@matthewmayer.co.uk>
2023-01-02 16:57:34 +0700

    flip male_prefix and female_prefix for alphabetical order

--- src/locales/pt_PT/person/prefix.ts
+++ src/locales/pt_PT/person/prefix.ts
@@ -2,4 +2,4 @@ import { mergeArrays } from './../../../internal/merge';
 import female_prefix from './female_prefix';
 import male_prefix from './male_prefix';
 
-export default mergeArrays(male_prefix, female_prefix);
+export default mergeArrays(female_prefix, male_prefix);
