27dff93aa27d755874aa5022c78f17ff8e9cf7e0
Matt Mayer <matt@lemi.travel>
2023-01-10 01:45:24 +0700

    feat(person): split prefix into gendered versions (#1665)

--- src/locales/en/person/index.ts
+++ src/locales/en/person/index.ts
@@ -8,11 +8,13 @@ import bio_patterns from './bio_patterns';
 import bio_supporter from './bio_supporter';
 import female_first_name from './female_first_name';
 import female_middle_name from './female_middle_name';
+import female_prefix from './female_prefix';
 import first_name from './first_name';
 import gender from './gender';
 import last_name from './last_name';
 import male_first_name from './male_first_name';
 import male_middle_name from './male_middle_name';
+import male_prefix from './male_prefix';
 import middle_name from './middle_name';
 import name_ from './name';
 import prefix from './prefix';
@@ -27,11 +29,13 @@ const person: PersonDefinitions = {
   bio_supporter,
   female_first_name,
   female_middle_name,
+  female_prefix,
   first_name,
   gender,
   last_name,
   male_first_name,
   male_middle_name,
+  male_prefix,
   middle_name,
   name: name_,
   prefix,
