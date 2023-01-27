d729fde2ecac49ecf529f7175e1242d15afccf34
Shinigami92 <chrissi92@hotmail.de>
2023-01-27 17:20:12 +0100

    docs: restyle

--- docs/.vitepress/components/api-docs/method.vue
+++ docs/.vitepress/components/api-docs/method.vue
@@ -21,20 +21,9 @@ function seeAlsoToUrl(see: string): string {
 
     <div v-html="props.method.description"></div>
 
-    <div v-if="props.method.since || props.method.sourcePath">
-      <p style="display: flex">
-        <em v-if="props.method.since"
-          >Available since v<span v-html="props.method.since"
-        /></em>
-        <a
-          v-if="props.method.sourcePath"
-          :href="sourceBaseUrl + props.method.sourcePath"
-          style="margin-left: auto"
-          target="_blank"
-          >Goto Source</a
-        >
-      </p>
-    </div>
+    <p v-if="props.method.since">
+      <em>Available since v{{ props.method.since }}</em>
+    </p>
 
     <MethodParameters
       v-if="props.method.parameters.length > 0"
@@ -57,6 +46,34 @@ function seeAlsoToUrl(see: string): string {
           <template v-else v-html="seeAlso" />
         </li>
       </ul>
+    </div>
+
+    <div v-if="props.method.sourcePath">
+      <a
+        v-if="props.method.sourcePath"
+        :href="sourceBaseUrl + props.method.sourcePath"
+        target="_blank"
+        style="
+          font-weight: bold;
+          font-size: 1.2em;
+          display: flex;
+          align-items: center;
+        "
+      >
+        Source
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          width="1.2em"
+          height="1.2em"
+          style="display: inline; margin-left: 0.3em"
+        >
+          <path
+            d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+          />
+        </svg>
+      </a>
     </div>
   </div>
 </template>
