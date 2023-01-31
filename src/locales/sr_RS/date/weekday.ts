8c4ef4ade53d466860cc6c972a6747641e5e6080
Bojan Dimitrovski <b.g.dimitrovski@gmail.com>
2023-01-31 23:11:43 +0100

    Fixes as per comments

--- src/locales/sr_RS/date/weekday.ts
+++ src/locales/sr_RS/date/weekday.ts
@@ -8,19 +8,5 @@ export default {
     'petak',
     'subota',
   ],
-  // Property "wide_context" is optional, if not set then "wide" will be used instead
-  // It is used to specify a word in context, which may differ from a stand-alone word
-  wide_context: [
-    'nedelja',
-    'ponedeljak',
-    'utorak',
-    'sreda',
-    'četvrtak',
-    'petak',
-    'subota',
-  ],
   abbr: ['ned', 'pon', 'uto', 'sre', 'čet', 'pet', 'sub'],
-  // Property "abbr_context" is optional, if not set then "abbr" will be used instead
-  // It is used to specify a word in context, which may differ from a stand-alone word
-  abbr_context: ['ned', 'pon', 'uto', 'sre', 'čet', 'pet', 'sub'],
 };
