e5f71792994f00945b4623fdc4510bf3624d234c
ST-DDT <ST-DDT@gmx.de>
2023-02-01 17:55:04 +0100

    chore: move source link to own section

--- docs/.vitepress/components/api-docs/method.vue
+++ docs/.vitepress/components/api-docs/method.vue
@@ -49,27 +49,30 @@ function seeAlsoToUrl(see: string): string {
     </div>
 
     <div v-if="props.method.sourcePath">
-      <h3>
-        <a
-          :href="sourceBaseUrl + props.method.sourcePath"
-          target="_blank"
-          class="source-link"
-        >
-          Source
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            width="1.2em"
-            height="1.2em"
-            class="source-link-icon"
+      <h3>Source</h3>
+      <ul>
+        <li>
+          <a
+            :href="sourceBaseUrl + props.method.sourcePath"
+            target="_blank"
+            class="source-link"
           >
-            <path
-              d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
-            />
-          </svg>
-        </a>
-      </h3>
+            View Source
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="1.2em"
+              height="1.2em"
+              class="source-link-icon"
+            >
+              <path
+                d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
