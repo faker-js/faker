d1e062926d00f9de0ba1ccaab054c1eb13211f62
Guilherme Minozzi <minozzi60@gmail.com>
2023-02-06 08:18:53 -0300

    feat(person): extend western zodiac sign to pt_BR

---
+++ src/locales/pt_BR/person/western_zodiac_sign.ts
@@ -0,0 +1,14 @@
+export default [
+  'Aquário',
+  'Peixes',
+  'Áries',
+  'Touro',
+  'Gêmeos',
+  'Câncer',
+  'Leão',
+  'Virgem',
+  'Libra',
+  'Escorpião',
+  'Sagitário',
+  'Capricórnio',
+];
