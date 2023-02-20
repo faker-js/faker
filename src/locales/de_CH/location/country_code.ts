a05c3e5fed805c5086a22841a81da0d86ab89386
DivisionByZero <leyla.jaehnig@gmx.de>
2023-02-20 20:44:36 +0100

    refactor(location)!: countryCode use ISO-3166-1 (#1720)
    Co-authored-by: Shinigami92 <chrissi92@hotmail.de>

--- src/locales/de_CH/location/country_code.ts
+++ src/locales/de_CH/location/country_code.ts
@@ -1,1 +1,10 @@
-export default ['CH', 'DE', 'AT', 'US', 'LI', 'HK', 'VN'];
+// [ISO_3166-1](https://en.wikipedia.org/wiki/ISO_3166-1)
+export default [
+  { alpha2: 'CH', alpha3: 'CHE' },
+  { alpha2: 'DE', alpha3: 'DEU' },
+  { alpha2: 'AT', alpha3: 'AUT' },
+  { alpha2: 'US', alpha3: 'USA' },
+  { alpha2: 'LI', alpha3: 'LIE' },
+  { alpha2: 'HK', alpha3: 'HKG' },
+  { alpha2: 'VN', alpha3: 'VNM' },
+];
