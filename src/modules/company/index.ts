5f95aca92158ce749215527c8d3691c19737e3f1
Shinigami <chrissi92@hotmail.de>
2023-02-21 19:21:15 +0100

    refactor(company)!: rename bs to buzz (#1860)

--- src/modules/company/index.ts
+++ src/modules/company/index.ts
@@ -78,7 +78,7 @@ export class CompanyModule {
   }
 
   /**
-   * Generates a random business catch phrase.
+   * Generates a random catch phrase that can be displayed to an end user.
    *
    * @example
    * faker.company.catchPhrase() // 'Upgradable systematic flexibility'
@@ -100,13 +100,33 @@ export class CompanyModule {
    * faker.company.bs() // 'cultivate synergistic e-markets'
    *
    * @since 2.0.1
+   *
+   * @deprecated Use `faker.company.buzzPhrase` instead.
    */
   bs(): string {
-    return [this.bsBuzz(), this.bsAdjective(), this.bsNoun()].join(' ');
+    deprecated({
+      deprecated: 'faker.company.bs',
+      proposed: 'faker.company.buzzPhrase',
+      since: '8.0',
+      until: '9.0',
+    });
+    return this.buzzPhrase();
   }
 
   /**
-   * Returns a random catch phrase adjective.
+   * Generates a random buzz phrase that can be used to demonstrate data being viewed by a manager.
+   *
+   * @example
+   * faker.company.buzzPhrase() // 'cultivate synergistic e-markets'
+   *
+   * @since 8.0.0
+   */
+  buzzPhrase(): string {
+    return [this.buzzVerb(), this.buzzAdjective(), this.buzzNoun()].join(' ');
+  }
+
+  /**
+   * Returns a random catch phrase adjective that can be displayed to an end user..
    *
    * @example
    * faker.company.catchPhraseAdjective() // 'Multi-tiered'
@@ -120,7 +140,7 @@ export class CompanyModule {
   }
 
   /**
-   * Returns a random catch phrase descriptor.
+   * Returns a random catch phrase descriptor that can be displayed to an end user..
    *
    * @example
    * faker.company.catchPhraseDescriptor() // 'composite'
@@ -134,7 +154,7 @@ export class CompanyModule {
   }
 
   /**
-   * Returns a random catch phrase noun.
+   * Returns a random catch phrase noun that can be displayed to an end user..
    *
    * @example
    * faker.company.catchPhraseNoun() // 'leverage'
@@ -152,10 +172,30 @@ export class CompanyModule {
    * faker.company.bsAdjective() // 'one-to-one'
    *
    * @since 2.0.1
+   *
+   * @deprecated Use `faker.company.buzzAdjective` instead.
    */
   bsAdjective(): string {
+    deprecated({
+      deprecated: 'faker.company.bsAdjective',
+      proposed: 'faker.company.buzzAdjective',
+      since: '8.0',
+      until: '9.0',
+    });
+    return this.buzzAdjective();
+  }
+
+  /**
+   * Returns a random buzz adjective that can be used to demonstrate data being viewed by a manager.
+   *
+   * @example
+   * faker.company.buzzAdjective() // 'one-to-one'
+   *
+   * @since 8.0.0
+   */
+  buzzAdjective(): string {
     return this.faker.helpers.arrayElement(
-      this.faker.definitions.company.bs_adjective
+      this.faker.definitions.company.buzz_adjective
     );
   }
 
@@ -166,10 +206,30 @@ export class CompanyModule {
    * faker.company.bsBuzz() // 'empower'
    *
    * @since 2.0.1
+   *
+   * @deprecated Use `faker.company.buzzVerb` instead.
    */
   bsBuzz(): string {
+    deprecated({
+      deprecated: 'faker.company.bsBuzz',
+      proposed: 'faker.company.buzzVerb',
+      since: '8.0',
+      until: '9.0',
+    });
+    return this.buzzVerb();
+  }
+
+  /**
+   * Returns a random buzz verb that can be used to demonstrate data being viewed by a manager.
+   *
+   * @example
+   * faker.company.buzzVerb() // 'empower'
+   *
+   * @since 8.0.0
+   */
+  buzzVerb(): string {
     return this.faker.helpers.arrayElement(
-      this.faker.definitions.company.bs_verb
+      this.faker.definitions.company.buzz_verb
     );
   }
 
@@ -180,10 +240,30 @@ export class CompanyModule {
    * faker.company.bsNoun() // 'paradigms'
    *
    * @since 2.0.1
+   *
+   * @deprecated Use `faker.company.buzzNoun` instead.
    */
   bsNoun(): string {
+    deprecated({
+      deprecated: 'faker.company.bsNoun',
+      proposed: 'faker.company.buzzNoun',
+      since: '8.0',
+      until: '9.0',
+    });
+    return this.buzzNoun();
+  }
+
+  /**
+   * Returns a random buzz noun that can be used to demonstrate data being viewed by a manager.
+   *
+   * @example
+   * faker.company.buzzNoun() // 'paradigms'
+   *
+   * @since 8.0.0
+   */
+  buzzNoun(): string {
     return this.faker.helpers.arrayElement(
-      this.faker.definitions.company.bs_noun
+      this.faker.definitions.company.buzz_noun
     );
   }
 }
