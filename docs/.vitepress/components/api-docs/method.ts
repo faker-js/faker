21684c691f8942a902e1f312516fda420d87af57
Matt Mayer <matt@lemi.travel>
2023-03-04 18:12:15 +0700

    Update docs/.vitepress/components/api-docs/method.ts
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- docs/.vitepress/components/api-docs/method.ts
+++ docs/.vitepress/components/api-docs/method.ts
@@ -5,7 +5,7 @@ export interface Method {
   readonly parameters: MethodParameter[];
   readonly returns: string;
   readonly examples: string; // HTML
-  readonly deprecated?: string;
+  readonly deprecated?: string; // HTML
   readonly since: string;
   readonly sourcePath: string; // URL-Suffix
   readonly seeAlsos: string[];
