d3dfec4556bbfd4ea484067f5cf645a1a2974478
munkeawtoast <munkeawtoast@outlook.com>
2023-03-12 22:57:04 +0700

    feat(locale): TH update 4

--- src/locales/th/person/index.ts
+++ src/locales/th/person/index.ts
@@ -6,11 +6,13 @@ import type { PersonDefinitions } from '../../..';
 import female_first_name from './female_first_name';
 import last_name from './last_name';
 import male_first_name from './male_first_name';
+import name_ from './name';
 
 const person: PersonDefinitions = {
   female_first_name,
   last_name,
   male_first_name,
+  name: name_,
 };
 
 export default person;
