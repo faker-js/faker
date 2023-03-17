3dfbd9ae4ecb7a36a99489798760175d3e1949e7
Mona,Lisa <matt@matthewmayer.co.uk>
2023-03-17 08:23:11 +0700

    revert accidental lv changes

--- src/locales/lv/person/female_last_name_patterns.ts
+++ src/locales/lv/person/female_last_name_patterns.ts
@@ -1,1 +1,7 @@
-export default [{ value: '{{person.female_last_name}}', weight: 1 }];
+export default [
+  { value: '{{person.female_last_name}}', weight: 8 },
+  {
+    value: '{{person.female_last_name}}-{{person.female_last_name}}',
+    weight: 2,
+  },
+];
--- src/locales/lv/person/male_last_name_patterns.ts
+++ src/locales/lv/person/male_last_name_patterns.ts
@@ -1,1 +1,4 @@
-export default [{ value: '{{person.male_last_name}}', weight: 1 }];
+export default [
+  { value: '{{person.male_last_name}}', weight: 8 },
+  { value: '{{person.male_last_name}}-{{person.male_last_name}}', weight: 2 },
+];
