6f0751113267615e8c342c663bc1cc95ff546017
Mona,Lisa <matt@matthewmayer.co.uk>
2023-03-29 15:29:25 +0700

    add base locale

--- src/locale/th.ts
+++ src/locale/th.ts
@@ -4,9 +4,10 @@
  */
 
 import { Faker } from '../faker';
+import base from '../locales/base';
 import en from '../locales/en';
 import th from '../locales/th';
 
 export const faker = new Faker({
-  locale: [th, en],
+  locale: [th, en, base],
 });
