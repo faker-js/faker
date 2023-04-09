1755e804dccb2efb95c83e1b45671d952c417879
Mona,Lisa <matt@matthewmayer.co.uk>
2023-04-10 00:53:59 +0700

    chore(business): remove old business data

--- src/locales/el/index.ts
+++ src/locales/el/index.ts
@@ -4,7 +4,6 @@
  */
 import type { LocaleDefinition } from '../..';
 import app from './app';
-import business from './business';
 import cell_phone from './cell_phone';
 import color from './color';
 import commerce from './commerce';
@@ -21,7 +20,6 @@ import team from './team';
 
 const el: LocaleDefinition = {
   app,
-  business,
   cell_phone,
   color,
   commerce,
