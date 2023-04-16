7e305d180de7f137eadb4b4723222ba298536bf7
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-04-16 15:50:11 +0200

    chore: update todo comment

--- test/location.spec.ts
+++ test/location.spec.ts
@@ -41,7 +41,7 @@ describe('location', () => {
   seededTests(faker, 'location', (t) => {
     t.it('street');
 
-    // todo - missing locale data
+    // TODO @xDivisionByZerox 2023-04-16: add street name locale data to `en`
     t.skip('streetName');
 
     t.it('buildingNumber');
