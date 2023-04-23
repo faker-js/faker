479093b39eb2a118f48843d3bfe58b33b7e2754b
Mona,Lisa <matt@matthewmayer.co.uk>
2023-04-23 17:06:24 +0700

    feat(locale): add ro_MD

---
+++ src/locales/ro_MD/metadata.ts
@@ -0,0 +1,13 @@
+import type { PreBuiltMetadataDefinitionsForCountry } from '../../definitions/metadata';
+
+const metadata: PreBuiltMetadataDefinitionsForCountry = {
+  title: 'Romanian (Moldova)',
+  code: 'ro_MD',
+  country: 'MD',
+  language: 'ro',
+  endonym: 'Română (Moldova)',
+  dir: 'ltr',
+  script: 'Latn',
+};
+
+export default metadata;
