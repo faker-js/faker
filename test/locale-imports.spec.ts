85f7e8dc2aa1d11de20e044069929bf2cfa95d6e
Shinigami92 <chrissi92@hotmail.de>
2023-04-26 11:36:18 +0200

    chore: eslint report-unused-disable-directives

--- test/locale-imports.spec.ts
+++ test/locale-imports.spec.ts
@@ -77,7 +77,6 @@ describe('locale imports', () => {
       });
 
       it(`should be possible to directly import('../src/locale/${locale}')`, async () => {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
         const { faker } = await import(`../src/locale/${locale}`);
 
         expect(faker).toBeDefined();
