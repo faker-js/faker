8cd196562be43d378bc82d0dfe8b003fd84c404f
DivisionByZero <leyla.jaehnig@gmx.de>
2023-04-27 17:33:43 +0200

    refactor(location)!: rename definition city to city_pattern (#2094)

--- src/locales/fr_CA/location/index.ts
+++ src/locales/fr_CA/location/index.ts
@@ -3,7 +3,7 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { LocationDefinitions } from '../../..';
-import city from './city';
+import city_pattern from './city_pattern';
 import default_country from './default_country';
 import postcode from './postcode';
 import state from './state';
@@ -11,7 +11,7 @@ import state_abbr from './state_abbr';
 import street_pattern from './street_pattern';
 
 const location: LocationDefinitions = {
-  city,
+  city_pattern,
   default_country,
   postcode,
   state,
--- src/locales/vi/location/index.ts
+++ src/locales/vi/location/index.ts
@@ -3,16 +3,16 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { LocationDefinitions } from '../../..';
-import city from './city';
 import city_name from './city_name';
+import city_pattern from './city_pattern';
 import country from './country';
 import default_country from './default_country';
 import postcode from './postcode';
 import street_pattern from './street_pattern';
 
 const location: LocationDefinitions = {
-  city,
   city_name,
+  city_pattern,
   country,
   default_country,
   postcode,
