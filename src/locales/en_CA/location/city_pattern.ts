94ae8407bb9ba62cd333d11be424dece3b4c347f
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-04-28 12:41:32 +0200

    refactor(locale): add city_name to city_patterns

--- src/locales/en_CA/location/city_pattern.ts
+++ src/locales/en_CA/location/city_pattern.ts
@@ -3,4 +3,5 @@ export default [
   '{{location.city_prefix}} {{person.firstName}}',
   '{{person.firstName}}{{location.city_suffix}}',
   '{{person.lastName}}{{location.city_suffix}}',
+  '{{location.city_name}}',
 ];
--- src/locales/nb_NO/location/city_pattern.ts
+++ src/locales/nb_NO/location/city_pattern.ts
@@ -1,1 +1,4 @@
-export default ['{{location.city_name}}{{location.city_suffix}}'];
+export default [
+  '{{location.city_name}}{{location.city_suffix}}',
+  '{{location.city_name}}',
+];
