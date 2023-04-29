b8ada1f315dd0b1d6026b06974dc4f039dcbc588
Mona,Lisa <matt@matthewmayer.co.uk>
2023-04-29 11:37:09 +0700

    fix(locale): es cities should be in city_name not city_suffix, remove extraneous city_suffix

--- src/locales/es_MX/location/index.ts
+++ src/locales/es_MX/location/index.ts
@@ -4,9 +4,8 @@
  */
 import type { LocationDefinitions } from '../../..';
 import building_number from './building_number';
+import city_name from './city_name';
 import city_pattern from './city_pattern';
-import city_prefix from './city_prefix';
-import city_suffix from './city_suffix';
 import country from './country';
 import default_country from './default_country';
 import postcode from './postcode';
@@ -20,9 +19,8 @@ import street_suffix from './street_suffix';
 
 const location: LocationDefinitions = {
   building_number,
+  city_name,
   city_pattern,
-  city_prefix,
-  city_suffix,
   country,
   default_country,
   postcode,
