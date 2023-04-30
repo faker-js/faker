9f7407c9fbfac14d47718b42453b374527f571eb
ST-DDT <ST-DDT@gmx.de>
2023-04-30 17:55:38 +0200

    revert: accidential change

--- src/definitions/definitions.ts
+++ src/definitions/definitions.ts
@@ -50,4 +50,4 @@ export type LocaleDefinition = {
   system?: SystemDefinition;
   vehicle?: VehicleDefinition;
   word?: WordDefinition;
-} & Record<string, Record<string, unknown>>;
+} & Record<string, Record<string, unknown> | undefined>;
