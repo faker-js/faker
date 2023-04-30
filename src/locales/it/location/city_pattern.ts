b72d52e5d5501579f6b98629d020292ceaa78c60
DivisionByZero <leyla.jaehnig@gmx.de>
2023-04-30 13:28:01 +0200

    refactor(locale): add city_name to city_patterns (#2104)

--- src/locales/it/location/city_pattern.ts
+++ src/locales/it/location/city_pattern.ts
@@ -3,4 +3,5 @@ export default [
   '{{location.city_prefix}} {{person.first_name}}',
   '{{person.first_name}} {{location.city_suffix}}',
   '{{person.last_name}} {{location.city_suffix}}',
+  '{{location.city_name}}',
 ];
