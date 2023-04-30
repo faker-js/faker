fef05a56647628d9ddb8d614a969d086cdc0a036
Mona,Lisa <matt@matthewmayer.co.uk>
2023-04-30 12:26:10 +0700

    fix(locale): rename city_prefix to city_name for en_NG, en_ZA, en_AU_ocker
    similar to #2109 for en_* locales

--- src/locales/en_NG/location/index.ts
+++ src/locales/en_NG/location/index.ts
@@ -3,16 +3,16 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { LocationDefinitions } from '../../..';
+import city_name from './city_name';
 import city_pattern from './city_pattern';
-import city_prefix from './city_prefix';
 import default_country from './default_country';
 import postcode from './postcode';
 import state from './state';
 import street_pattern from './street_pattern';
 
 const location: LocationDefinitions = {
+  city_name,
   city_pattern,
-  city_prefix,
   default_country,
   postcode,
   state,
