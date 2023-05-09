f5dd57bbb548639a2fe1ffdfaaf3138abd39875f
Shinigami92 <chrissi92@hotmail.de>
2023-05-09 13:40:20 +0200

    update locale-data

--- test/locale-data.spec.ts
+++ test/locale-data.spec.ts
@@ -8,11 +8,9 @@ function checkLocaleData(data: unknown) {
       expect(data).not.toContainDuplicates();
     });
   } else if (typeof data === 'object' && data != null) {
-    for (const [nestedKey, nestedData] of Object.entries(data)) {
-      describe(`${nestedKey}`, () => {
-        checkLocaleData(nestedData);
-      });
-    }
+    describe.each(Object.entries(data))('%s', (nestedData) => {
+      checkLocaleData(nestedData);
+    });
   } else {
     it.skip('primitives cannot be tested');
   }
