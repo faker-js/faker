a6109290e8e493c00b84fe2ee319b20752d2ed95
Mona,Lisa <matt@matthewmayer.co.uk>
2023-05-09 13:22:54 +0700

    feat(metadata): add method to access metadata

--- src/faker.ts
+++ src/faker.ts
@@ -1,4 +1,4 @@
-import type { LocaleDefinition } from './definitions';
+import type { LocaleDefinition, MetadataDefinition } from './definitions';
 import { FakerError } from './errors/faker-error';
 import { deprecated } from './internal/deprecated';
 import type { Mersenne } from './internal/mersenne/mersenne';
@@ -456,6 +456,21 @@ export class Faker {
     this._mersenne.seed(seed);
 
     return seed;
+  }
+
+  /**
+   * Returns an object with metadata about the current locale
+   *
+   * @returns An object including the locale title, code, language, endonym (native name), direction (left to right, or right to left), script, and optional country.
+   *
+   * @example
+   * import { faker, fakerES_MX } from '@faker-js/faker';
+   * // const { faker, fakerES_MX } = require("@faker-js/faker")
+   * faker.metadata(); // { title: 'English', code: 'en', language: 'en', endonym: 'English', dir: 'ltr', script: 'Latn' }
+   * fakerES_MX.metadata(); // { title: 'Spanish (Mexico)', code: 'es_MX', language: 'es', endonym: 'Español (México)', dir: 'ltr', script: 'Latn', country: 'MX' }
+   */
+  metadata(): MetadataDefinition {
+    return this.rawDefinitions.metadata;
   }
 
   // Pure JS backwards compatibility
