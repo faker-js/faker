93995ded03588e9fbd09282bfb2afdde01b34985
KeisukeYamashita <19yamashita15@gmail.com>
2023-06-01 11:32:25 +0900

    fix(locale): max to 9 for 丁目,番地 and 号
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- src/locales/ja/location/street_pattern.ts
+++ src/locales/ja/location/street_pattern.ts
@@ -1,3 +1,3 @@
 export default [
-  '{{number.int({"max": 10, "min": 1})}}丁目{{number.int({"max": 10, "min": 1})}}番{{number.int({"max": 10, "min": 1})}}号',
+  '{{number.int({"max": 9, "min": 1})}}丁目{{number.int({"max": 9, "min": 1})}}番{{number.int({"max": 9, "min": 1})}}号',
 ];
