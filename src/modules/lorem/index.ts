30e73fbb1edb47a779b48e452565c7e820be543f
Alexander <alexander.ikonomou.19@gmail.com>
2023-06-28 23:54:20 +0200

    fix example in comment of lorem.text

--- src/modules/lorem/index.ts
+++ src/modules/lorem/index.ts
@@ -332,7 +332,7 @@ export class LoremModule {
    * // Quis ut dolor dolores facilis possimus tempore voluptates.
    * // Iure nam officia optio cumque.
    * // Dolor tempora iusto.'
-   * faker.lorem.text(14) // 'Doloribus aut.' (14 characters)
+   * faker.lorem.text({length: 14}) // 'Doloribus aut.' (14 characters)
    * faker.lorem.text({min: 10, max: 15}) // 'autem non quis.' (15 characters)
    * faker.lorem.text({min: 10, max: 12}) // 'autem non.' (10 characters)
    * faker.lorem.text({min: 3, max: 10}) // 'autem.' (6 characters)
