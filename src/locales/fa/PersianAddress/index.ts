b5bcfd13e474f458a77fb85174145a0f0c186231
Mostafa Khazaei <47237818+khazaeimostafa@users.noreply.github.com>
2023-07-24 10:47:04 +0330

    Create index.ts

---
+++ src/locales/fa/PersianAddress/index.ts
@@ -0,0 +1,11 @@
+import type { PersianAddress } from '../../..';
+import City from './City';
+import Street from './Street';
+import PostalCode from './PostalCode';
+const PersianAddress: DateDefinition = {
+  City,
+  Street,
+  PostalCode
+};
+
+export default PersianAddress;
