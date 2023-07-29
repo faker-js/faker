36bfe6b9f89f10c85102c28f10b4c9f6de65afc2
Shinigami92 <chrissi92@hotmail.de>
2023-07-29 11:16:59 +0200

    test: move files to modules

--- test/modules/person.spec.ts
+++ test/modules/person.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
-import { faker, fakerAZ, fakerMK, fakerUK, Sex } from '../src';
-import { seededTests } from './support/seededRuns';
-import { times } from './support/times';
+import { faker, fakerAZ, fakerMK, fakerUK, Sex } from '../../src';
+import { seededTests } from './../support/seededRuns';
+import { times } from './../support/times';
 
 const NON_SEEDED_BASED_RUN = 5;
 
