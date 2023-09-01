4461585b34c353d0328e599ed9eb494752452dde
CoolPlayLin <CoolPlayLin@qq.com>
2023-09-01 23:34:41 +0800

    0

--- src/locales/zh_CN/finance/credit_card/index.ts
+++ src/locales/zh_CN/finance/credit_card/index.ts
@@ -4,12 +4,10 @@
  */
 import type { FinanceDefinition } from '../../../..';
 import mastercard from './mastercard';
-import unionpay from './unionpay';
 import visa from './visa';
 
 const credit_card: FinanceDefinition['credit_card'] = {
   mastercard,
-  unionpay,
   visa,
 };
 
--- src/locales/zh_CN/index.ts
+++ src/locales/zh_CN/index.ts
@@ -7,6 +7,7 @@ import animal from './animal';
 import color from './color';
 import company from './company';
 import date from './date';
+import finance from './finance';
 import internet from './internet';
 import location from './location';
 import metadata from './metadata';
@@ -20,6 +21,7 @@ const zh_CN: LocaleDefinition = {
   color,
   company,
   date,
+  finance,
   internet,
   location,
   metadata,
