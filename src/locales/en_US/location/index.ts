491d3191213e2ceaaee46dcc50ac25c3995ba2d5
DivisionByZero <leyla.jaehnig@gmx.de>
2023-10-05 19:49:59 +0200

    feat(locale): add street_name to `en_US`, `en_GB` and `en` (#2371)

--- src/locales/en_US/location/index.ts
+++ src/locales/en_US/location/index.ts
@@ -7,6 +7,7 @@ import city_pattern from './city_pattern';
 import county from './county';
 import default_country from './default_country';
 import postcode_by_state from './postcode_by_state';
+import street_name from './street_name';
 import street_pattern from './street_pattern';
 
 const location: LocationDefinition = {
@@ -14,6 +15,7 @@ const location: LocationDefinition = {
   county,
   default_country,
   postcode_by_state,
+  street_name,
   street_pattern,
 };
 
