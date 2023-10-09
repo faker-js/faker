3c9b4f8a93579c2690f0445997a613afbacdc3c9
ST-DDT <ST-DDT@gmx.de>
2023-10-09 12:24:05 +0200

    infra(unicorn): prefer-number-properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -77,7 +77,6 @@ module.exports = defineConfig({
     'unicorn/prefer-module': 'off',
     'unicorn/prefer-native-coercion-functions': 'off',
     'unicorn/prefer-negative-index': 'off',
-    'unicorn/prefer-number-properties': 'off',
     'unicorn/prefer-optional-catch-binding': 'off',
     'unicorn/prefer-spread': 'off',
     'unicorn/prefer-string-slice': 'off',
