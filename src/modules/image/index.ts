453a312f7cda08d1871abdaed9cbd78f2cccc841
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-10-14 10:01:22 -0300

    chore(image): move defaults to last part
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/modules/image/index.ts
+++ src/modules/image/index.ts
@@ -193,7 +193,7 @@ export class ImageModule {
    * @param options.width The width of the image. Defaults to random integer between `1` and `3999`.
    * @param options.height The height of the image. Defaults to random integer between `1` and `3999`.
    * @param options.grayscale Whether the image should be grayscale. Defaults to a random boolean value.
-   * @param options.blur Whether the image should be blurred. Defaults to a random integer from `0` to `10`. `0` disables the blur.
+   * @param options.blur Whether the image should be blurred. `0` disables the blur. Defaults to a random integer from `0` to `10`.
    *
    * @example
    * faker.image.urlPicsumPhotos() // 'https://picsum.photos/seed/NWbJM2B/640/480'
