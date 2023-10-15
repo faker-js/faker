a65723e4fa2fcdb94022897045072505648cda6e
ST-DDT <ST-DDT@gmx.de>
2023-10-15 10:48:44 +0200

    docs: improve jsdocs

--- test/internal/mersenne-test-utils.ts
+++ test/internal/mersenne-test-utils.ts
@@ -2,10 +2,12 @@
 
 /**
  * The maximum value that can be returned by `MersenneTwister19937.genrandReal2()`.
+ * This is the max possible value with 32 bits of precision that is less than 1.
  */
 export const TWISTER_32CO_MAX_VALUE = 0.9999999997671694;
 /**
  * The maximum value that can be returned by `MersenneTwister19937.genrandRes53()`.
+ * This is the max possible value with 53 bits of precision that is less than 1.
  */
 export const TWISTER_53CO_MAX_VALUE = 0.9999999999999999;
 // Re-exported because the value might change in the future
