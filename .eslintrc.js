980f230947ca250086aedf37160abe86c766f969
ST-DDT <ST-DDT@gmx.de>
2023-10-17 22:18:04 +0200

    infra(typescript-eslint): prefer-regexp-exec (#2466)

--- .eslintrc.js
+++ .eslintrc.js
@@ -112,6 +112,7 @@ module.exports = defineConfig({
       'error',
       { blankLine: 'always', prev: 'block-like', next: '*' },
     ],
+    '@typescript-eslint/prefer-regexp-exec': 'error',
     '@typescript-eslint/restrict-template-expressions': [
       'error',
       { allowNumber: true, allowBoolean: true },
