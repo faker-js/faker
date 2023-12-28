0c5df0ab384405e3606e9b7e375ed06e23ad1d3e
Shinigami92 <chrissi92@hotmail.de>
2023-12-28 15:45:59 +0100

    Skip specific tests when node v14

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ const VITEST_SEQUENCE_SEED = Date.now();
 
 console.log('VITEST_SEQUENCE_SEED', VITEST_SEQUENCE_SEED);
 
+const [nodeVersionMajor] = process.versions.node.split('.').map(Number);
+const excludedTests: string[] = [];
+if (nodeVersionMajor < 16) {
+  excludedTests.push(
+    'test/scripts/apidoc/module.spec.ts',
+    'test/scripts/apidoc/signature.spec.ts',
+    'test/scripts/apidoc/verify-jsdoc-tags.spec.ts'
+  );
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
@@ -20,6 +30,15 @@ export default defineConfig({
       seed: VITEST_SEQUENCE_SEED,
       shuffle: true,
     },
+    exclude: [
+      // should be originally `...configDefaults.exclude` from `'vitest/config'`, but esm...
+      'node_modules',
+      'dist',
+      '.idea',
+      '.git',
+      '.cache',
+      ...excludedTests,
+    ],
     onConsoleLog(log, type) {
       if (
         type === 'stderr' &&
