01b5d2acc509ab01dc4be2ba8fdfba7fb72044c2
Matt Mayer <152770+matthewmayer@users.noreply.github.com>
2024-01-30 07:30:24 +0700

    docs: clarify that the result of faker.date.betweens() is sorted
    Inspired by https://github.com/faker-js/faker/discussions/2633 - wasn't obvious from the docs that the result was sorted.

--- src/modules/date/index.ts
+++ src/modules/date/index.ts
@@ -409,7 +409,7 @@ export class SimpleDateModule extends SimpleModuleBase {
   }
 
   /**
-   * Generates random dates between the given boundaries.
+   * Generates random dates between the given boundaries. The dates will be returned in an array sorted from oldest to newest.
    *
    * @param options The optional options object.
    * @param options.from The early date boundary.
