5a0019f196999e2bf4694a666f1194ebd5f92100
ST-DDT <ST-DDT@gmx.de>
2024-02-05 23:18:55 +0100

    Update src/modules/date/index.ts

--- src/modules/date/index.ts
+++ src/modules/date/index.ts
@@ -409,7 +409,7 @@ export class SimpleDateModule extends SimpleModuleBase {
   }
 
   /**
-   * Generates random dates between the given boundaries. The dates will be returned in an array sorted from oldest to newest.
+   * Generates random dates between the given boundaries. The dates will be returned in an array sorted in chronological order.
    *
    * @param options The optional options object.
    * @param options.from The early date boundary.
