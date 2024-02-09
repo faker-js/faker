aa183ad7cfe6346248801c280d89f9fad7c3cfee
Shinigami <chrissi92@hotmail.de>
2024-02-09 17:36:49 +0100

    chore(deps): replace vitest v1 deprecations (#2590)

--- test/faker.spec.ts
+++ test/faker.spec.ts
@@ -1,4 +1,4 @@
-import type { SpyInstance } from 'vitest';
+import type { MockInstance } from 'vitest';
 import { describe, expect, it, vi } from 'vitest';
 import { faker, Faker } from '../src';
 import { FakerError } from '../src/errors/faker-error';
@@ -14,7 +14,7 @@ describe('faker', () => {
   });
 
   it('should not log anything on startup', () => {
-    const spies: SpyInstance[] = keys(console)
+    const spies: MockInstance[] = keys(console)
       .filter((key) => typeof console[key] === 'function')
       .map((methodName) =>
         vi.spyOn(console, methodName as keyof typeof console)
