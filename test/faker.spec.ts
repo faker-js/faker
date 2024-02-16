2bef0218b016aedd2a6f58dd0d81775b8a4bda85
Shinigami92 <chrissi92@hotmail.de>
2024-02-16 17:59:43 +0100

    revert

--- test/faker.spec.ts
+++ test/faker.spec.ts
@@ -20,7 +20,6 @@ describe('faker', () => {
         vi.spyOn(console, methodName as keyof typeof console)
       );
 
-    // @ts-expect-error: eat it
     (await import('..')).default;
 
     new Faker({ locale: { metadata: { title: '' } } });
