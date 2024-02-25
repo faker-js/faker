c9e817012a507b3545488b7f1cd8a76b6c20c21e
ST-DDT <ST-DDT@gmx.de>
2024-02-25 22:22:45 +0100

    infra(unicorn): prefer-top-level-await (#2680)

--- scripts/apidoc.ts
+++ scripts/apidoc.ts
@@ -3,13 +3,5 @@
 import { generate } from './apidoc/generate';
 import { initMarkdownRenderer } from './apidoc/markdown';
 
-async function build(): Promise<void> {
-  await initMarkdownRenderer();
-  await generate();
-}
-
-build().catch((error) => {
-  // Workaround until top level await is available
-  console.error(error);
-  process.exit(1);
-});
+await initMarkdownRenderer();
+await generate();
