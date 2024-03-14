c5a4194000194f8abbd8956c7647c851cce9ef0b
Miraziz.Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2024-03-15 01:30:45 +0500

    Revert "add commerce to the Uzbek locale"
    This reverts commit d1272271f4520f6cac9cccacfb1c74b315963536.

--- src/locales/uz_UZ_latin/index.ts
+++ src/locales/uz_UZ_latin/index.ts
@@ -4,14 +4,12 @@
  */
 import type { LocaleDefinition } from '../..';
 import color from './color';
-import commerce from './commerce';
 import date from './date';
 import metadata from './metadata';
 import person from './person';
 
 const uz_UZ_latin: LocaleDefinition = {
   color,
-  commerce,
   date,
   metadata,
   person,
