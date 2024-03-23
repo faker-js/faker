915746cfcbacf6306a5e8f5e3d6acbc00a0070b3
ST-DDT <ST-DDT@gmx.de>
2024-03-24 00:00:39 +0100

    chore: apply suggestions

--- src/modules/date/index.ts
+++ src/modules/date/index.ts
@@ -5,14 +5,16 @@ import { SimpleModuleBase } from '../../internal/module-base';
 import { assertLocaleData } from '../../locale-proxy';
 
 /**
- * Converts date passed as a string, number or Date to a valid Date object.
- * If nothing or a non-parsable value is passed, then it will take the value from the given fallback.
+ * Converts a date passed as a `string`, `number` or `Date` to a valid `Date` object.
  *
  * @param date The date to convert.
- * @param name The parameter name of the date.
+ * @param name The reference name used for error messages. Defaults to `'refDate'`.
+ *
+ * @throws If the given date is invalid.
  */
 function toDate(date: string | Date | number, name: string = 'refDate'): Date {
   const converted = new Date(date);
+
   if (Number.isNaN(converted.valueOf())) {
     throw new FakerError(`Invalid ${name} date: ${date.toString()}`);
   }
