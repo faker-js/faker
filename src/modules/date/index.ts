d6b222b8c1531201bf8c0f3e54a0a50ee41d2c19
Matt Mayer <matt@matthewmayer.co.uk>
2024-03-26 17:56:20 +0700

    add TODOs

--- src/modules/date/index.ts
+++ src/modules/date/index.ts
@@ -177,6 +177,7 @@ export class SimpleDateModule extends SimpleModuleBase {
      */
     to: string | Date | number;
   }): Date {
+    // TODO - consider removing in v10 as this check is only needed in JS
     if (options == null || options.from == null || options.to == null) {
       throw new FakerError(
         'Must pass an options object with `from` and `to` values.'
@@ -250,6 +251,7 @@ export class SimpleDateModule extends SimpleModuleBase {
           max: number;
         };
   }): Date[] {
+    // TODO - consider removing in v10 as this check is only needed in JS
     if (options == null || options.from == null || options.to == null) {
       throw new FakerError(
         'Must pass an options object with `from` and `to` values.'
