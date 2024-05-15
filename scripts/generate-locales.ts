4e02742b08453975a61073902765c4da65668616
xDivisionByZerox <leyla.jaehnig@gmx.de>
2024-05-15 19:41:30 +0200

    refactor(locale): normalize finance data

--- scripts/generate-locales.ts
+++ scripts/generate-locales.ts
@@ -313,7 +313,6 @@ async function normalizeLocaleFile(filePath: string, definitionKey: string) {
   const legacyDefinitions = ['app', 'cell_phone', 'team'];
   const definitionsToSkip = [
     'date',
-    'finance',
     'internet',
     'location',
     'lorem',
