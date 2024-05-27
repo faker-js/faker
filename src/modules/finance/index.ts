125c2bc510a1c1e89be35d0430f8721167d71342
David Simão <madoke@users.noreply.github.com>
2024-05-27 22:56:45 +0100

    Update src/modules/finance/index.ts
    Co-authored-by: DivisionByZero <leyla.jaehnig@gmx.de>

--- src/modules/finance/index.ts
+++ src/modules/finance/index.ts
@@ -492,7 +492,7 @@ export class FinanceModule extends ModuleBase {
    * Generates a random Bitcoin address.
    *
    * @param options An options object.
-   * @param options.type The bitcoin address type (`'legacy'`, `'sewgit'`, `'bech32'` or `'taproot'`). Defaults to `'legacy'`.
+   * @param options.type The bitcoin address type (`'legacy'`, `'sewgit'`, `'bech32'` or `'taproot'`). Defaults to a random address type.
    * @param options.network The bitcoin network (`'mainnet'` or `'testnet'`). Defaults to `'mainnet'`.
    *
    * @example
