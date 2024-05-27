533bfe121d7501083bb93c0a0c344ac8e40c0d72
David Simao <madoke@gmail.com>
2024-05-28 00:15:29 +0100

    declare BitcoinAddressOptions type

--- src/modules/finance/bitcoin.ts
+++ src/modules/finance/bitcoin.ts
@@ -14,7 +14,7 @@ export enum BitcoinNetwork {
 
 type BitcoinAddressOptions = {
   prefix: Record<BitcoinNetwork, string>;
-  length: { min: number; max: number };
+  length: { min: number; max: number; };
   casing: Casing;
   exclude: string;
 };
@@ -23,13 +23,13 @@ export const BitcoinAddressSpecs: Record<BitcoinAddressType, BitcoinAddressOptio
   [BitcoinAddressType.Legacy]: {
     prefix: { [BitcoinNetwork.Mainnet]: '1', [BitcoinNetwork.Testnet]: 'm' },
     length: { min: 26, max: 34 },
-    casing: 'mixed' as Casing,
+    casing: 'mixed',
     exclude: '0OIl',
   },
   [BitcoinAddressType.Segwit]: {
     prefix: { [BitcoinNetwork.Mainnet]: '3', [BitcoinNetwork.Testnet]: '2' },
     length: { min: 26, max: 34 },
-    casing: 'mixed' as Casing,
+    casing: 'mixed',
     exclude: '0OIl',
   },
   [BitcoinAddressType.Bech32]: {
@@ -38,7 +38,7 @@ export const BitcoinAddressSpecs: Record<BitcoinAddressType, BitcoinAddressOptio
       [BitcoinNetwork.Testnet]: 'tb1',
     },
     length: { min: 42, max: 42 },
-    casing: 'lower' as Casing,
+    casing: 'lower',
     exclude: '1bBiIoO',
   },
   [BitcoinAddressType.Taproot]: {
@@ -47,7 +47,7 @@ export const BitcoinAddressSpecs: Record<BitcoinAddressType, BitcoinAddressOptio
       [BitcoinNetwork.Testnet]: 'tb1p',
     },
     length: { min: 62, max: 62 },
-    casing: 'lower' as Casing,
+    casing: 'lower',
     exclude: '1bBiIoO',
   },
 };
