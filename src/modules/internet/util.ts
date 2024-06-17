43fbe3623647fe608c403c23ecccade73055b8ad
Zyb Jared Valdez <zybjared@gmail.com>
2024-06-18 07:47:51 +0800

    docs and lint errors for toBase64Url

--- src/modules/internet/util.ts
+++ src/modules/internet/util.ts
@@ -1,6 +1,17 @@
+/**
+ *
+ * @param input String to encode to Base64.
+ * @returns Base64 URL encoded string.
+ * @example const encodedHeader = toBase64Url(JSON.stringify(header));
+ */
 export function toBase64Url(input: string): string {
   const utf8Bytes = new TextEncoder().encode(input);
-  const binaryString = Array.from(utf8Bytes, (byte) => String.fromCharCode(byte)).join('');
+  const binaryString = Array.from(utf8Bytes, (byte) =>
+    String.fromCodePoint(byte)
+  ).join('');
   const base64 = btoa(binaryString);
- return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+  return base64
+    .replaceAll(/\+/g, '-')
+    .replaceAll(/\//g, '_')
+    .replaceAll(/=+$/g, '');
 }
