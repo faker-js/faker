1931bf3643d8e16c9cc89ec69f2e943287a78027
Marcelo Shima <marceloshima@gmail.com>
2024-09-07 16:03:04 -0300

    Update test/faker.spec.ts
    Co-authored-by: Shinigami <chrissi92@hotmail.de>

--- test/faker.spec.ts
+++ test/faker.spec.ts
@@ -18,6 +18,7 @@ describe('faker', () => {
       .filter((key) => typeof console[key] === 'function')
       .map((methodName) => vi.spyOn(console, methodName));
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const file: unknown = await import('..');
     expect(file).toBeDefined();
