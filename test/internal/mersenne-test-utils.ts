7f2b6422497e70752f45d0c8e5e771a7a231b533
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-12 15:41:58 +0100

    fix: set single export on mersenne test utils

--- test/internal/mersenne-test-utils.ts
+++ test/internal/mersenne-test-utils.ts
@@ -4,16 +4,14 @@
  * The maximum value that can be returned by `MersenneTwister19937.genrandReal2()`.
  * This is the max possible value with 32 bits of precision that is less than 1.
  */
-const TWISTER_32CO_MAX_VALUE = 0.9999999997671694;
+export const TWISTER_32CO_MAX_VALUE = 0.9999999997671694;
 /**
  * The maximum value that can be returned by `MersenneTwister19937.genrandRes53()`.
  * This is the max possible value with 53 bits of precision that is less than 1.
  */
-const TWISTER_53CO_MAX_VALUE = 0.9999999999999999;
+export const TWISTER_53CO_MAX_VALUE = 0.9999999999999999;
 // Re-exported because the value might change in the future
 /**
  * The maximum value that can be returned by `next()`.
  */
-const MERSENNE_MAX_VALUE = TWISTER_32CO_MAX_VALUE;
-
-export { MERSENNE_MAX_VALUE, TWISTER_32CO_MAX_VALUE, TWISTER_53CO_MAX_VALUE };
+export const MERSENNE_MAX_VALUE = TWISTER_32CO_MAX_VALUE;
