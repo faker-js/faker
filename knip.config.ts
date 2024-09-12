7dff3e9947a82a76bb2195445c45f66a97bed429
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-12 15:18:58 +0100

    refactor: remove workspaces on knip.config

--- knip.config.ts
+++ knip.config.ts
@@ -1,14 +1,10 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  workspaces: {
-    '.': {
-      entry: ['src/**/*', 'test/**/*'],
-      ignore: ['docs/**/*', 'scripts/**/*', '.prettierrc.d.ts', '.github/**/*'],
-      ignoreDependencies: ['@actions/github', '@vueuse/core', 'vue'],
-      ignoreBinaries: [],
-    },
-  },
+  entry: ['src/**/*', 'test/**/*'],
+  ignore: ['docs/**/*', 'scripts/**/*', '.prettierrc.d.ts', '.github/**/*'],
+  ignoreDependencies: ['@actions/github', '@vueuse/core', 'vue'],
+  ignoreBinaries: [],
 };
 
 export default config;
