c6c28f892e1ef946fee7f0d4ef4f02b3c88e8c57
Lucas van Staden <sales@proxiblue.com.au>
2024-10-24 20:38:49 +0800

    Update postcode.ts
    Add in en_AU localisation for state postcodes

--- src/locales/en_AU/location/postcode.ts
+++ src/locales/en_AU/location/postcode.ts
@@ -1,1 +1,34 @@
-export default ['####'];
+export default {
+  NSW: [
+      '{{number.int({"min": 2000,"max": 2599})}}',
+      '{{number.int({"min": 2619,"max": 2899})}}'
+  ],
+  ACT: [
+      '{{number.int({"min": 2600,"max": 2618})}}',
+      '{{number.int({"min": 2900,"max": 2920})}}'
+  ],
+  VIC: [
+      '{{number.int({"min": 3000,"max": 3999})}}',
+      '{{number.int({"min": 8000,"max": 8999})}}'
+  ],
+  QLD: [
+      '{{number.int({"min": 4000,"max": 4999})}}',
+      '{{number.int({"min": 9000,"max": 9999})}}'
+  ],
+  SA: [
+      '{{number.int({"min": 5000,"max": 5799})}}',
+      '{{number.int({"min": 5800,"max": 5999})}}'
+  ],
+  WA: [
+      '{{number.int({"min": 6000,"max": 6797})}}',
+      '{{number.int({"min": 6800,"max": 6999})}}'
+  ],
+  TAS: [
+      '{{number.int({"min": 7000,"max": 7799})}}',
+      '{{number.int({"min": 7800,"max": 7999})}}'
+  ],
+  NT: [
+      '{{number.int({"min": 0800,"max": 0899})}}',
+      '{{number.int({"min": 0900,"max": 0999})}}'
+  ]
+};
