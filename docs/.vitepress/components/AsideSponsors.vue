ddce05a7d2d009105accd9eeffbe324d2a307f20
Shinigami92 <chrissi92@hotmail.de>
2025-03-04 17:21:50 +0100

    docs: add sponsors

---
+++ docs/.vitepress/components/AsideSponsors.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Sponsors } from 'vitepress/dist/client/theme-default/components/VPSponsors.vue';
+import { VPDocAsideSponsors } from 'vitepress/theme';
+import type { Ref } from 'vue';
+import { ref } from 'vue';
+import { bronze, gold, individuals, silver } from './sponsors.json';
+
+const sponsors: Ref<Sponsors[]> = ref([
+  { tier: 'Individuals (>>$500)', size: 'big', items: individuals },
+  { tier: 'Gold ($500)', size: 'small', items: gold },
+  { tier: 'Silver ($250)', size: 'mini', items: silver },
+  { tier: 'Bronze ($100)', size: 'xmini', items: bronze },
+]);
+</script>
+
+<template>
+  <VPDocAsideSponsors :data="sponsors" />
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,22 +1,20 @@
 import DefaultTheme from 'vitepress/theme';
 import { defineAsyncComponent, h } from 'vue';
+import AsideSponsors from '../components/AsideSponsors.vue';
 import './index.css';
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   Layout() {
-    return h(
-      DefaultTheme.Layout,
-      null,
-      __BANNER__
-        ? {
-            'layout-top': () =>
-              h(
-                defineAsyncComponent(() => import('../components/Banner.vue')),
-                { version: __BANNER__ }
-              ),
-          }
-        : null
-    );
+    return h(DefaultTheme.Layout, null, {
+      'layout-top': __BANNER__
+        ? () =>
+            h(
+              defineAsyncComponent(() => import('../components/Banner.vue')),
+              { version: __BANNER__ }
+            )
+        : null,
+      'aside-ads-before': () => h(AsideSponsors),
+    });
   },
 };
