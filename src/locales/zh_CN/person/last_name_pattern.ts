74f3ab82a1c6b836a6039115b9d17d89903b1682
Matt Mayer <matt@matthewmayer.co.uk>
2025-04-06 18:25:47 +0700

    fix(locale): weight common chinese surnames more frequently

--- src/locales/zh_CN/person/last_name_pattern.ts
+++ src/locales/zh_CN/person/last_name_pattern.ts
@@ -1,3 +1,6 @@
 export default {
-  generic: [{ value: '{{person.last_name.generic}}', weight: 1 }],
+  generic: [
+    { value: '{{person.last_name.generic_common}}', weight: 8 },
+    { value: '{{person.last_name.generic}}', weight: 2 },
+  ],
 };
