93999d1f5ce8defa01943b3b86ce13a5829af32d
yyz945947732 <945947732@qq.com>
2025-04-15 09:35:04 +0800

    feat(local): add local zn_CN book.

--- src/locales/zh_CN/index.ts
+++ src/locales/zh_CN/index.ts
@@ -5,6 +5,7 @@
 import type { LocaleDefinition } from '../..';
 import airline from './airline';
 import animal from './animal';
+import book from './book';
 import color from './color';
 import commerce from './commerce';
 import company from './company';
@@ -31,6 +32,7 @@ import word from './word';
 const zh_CN: LocaleDefinition = {
   airline,
   animal,
+  book,
   color,
   commerce,
   company,
