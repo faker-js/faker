bc9bb219be5a03c1bd9a2e82e509c93982b2e9df
daniel_shvarts <daniel04sh@gmail.com>
2025-06-19 17:02:44 +0300

    add verb

--- src/locales/pt_BR/word/index.ts
+++ src/locales/pt_BR/word/index.ts
@@ -1,9 +1,11 @@
 import type { WordDefinition } from '../../../definitions';
 import adjective from './adjective';
 import noun from './noun';
+import verb from './verb';
 
 const word: WordDefinition = {
   noun,
+  verb,
   adjective,
 };
 
