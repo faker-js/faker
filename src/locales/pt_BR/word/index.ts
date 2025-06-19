10c1a508f26f57ab0b412e39984376317fb766d1
daniel_shvarts <daniel04sh@gmail.com>
2025-06-19 16:53:55 +0300

    add word adjective and noun

---
+++ src/locales/pt_BR/word/index.ts
@@ -0,0 +1,10 @@
+import type { WordDefinition } from '../../../definitions';
+import adjective from './adjective';
+import noun from './noun';
+
+const word: WordDefinition = {
+  noun,
+  adjective,
+};
+
+export default word;
