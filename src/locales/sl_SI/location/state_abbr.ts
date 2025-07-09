96565ade36ac0d09b35c6705dc59e19df6994c3f
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2025-07-09 18:45:10 +0200

    Add location

---
+++ src/locales/sl_SI/location/state_abbr.ts
@@ -0,0 +1,14 @@
+export default [
+  'CE', // Celjska
+  'GO', // Goriška
+  'GZ', // Gorenjska
+  'JV', // Jugovzhodna Slovenija
+  'KA', // Koroška
+  'KR', // Kraška (Obalno-kraška)
+  'MB', // Podravska (Maribor)
+  'MS', // Pomurska (Murska Sobota)
+  'PO', // Posavska
+  'PS', // Primorsko-notranjska
+  'SG', // Savinjska
+  'ZA', // Zasavska
+];
