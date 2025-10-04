a458df6e4be87270f645c6982fbbb98dc95e8ba6
mazyar-kawa02 <mazyar.sl2000@gmail.com>
2025-10-04 19:44:56 +0300

    feat(locale): add ku_kmr_latin locale

---
+++ src/locales/ku_kmr_latin/metadata.ts
@@ -0,0 +1,12 @@
+import type { PreBuiltMetadataDefinition } from '../../definitions/metadata';
+
+const metadata: PreBuiltMetadataDefinition = {
+  title: 'Kurdish Kurmanji (Latin)',
+  code: 'kmr',
+  language: 'kmr',
+  endonym: 'کوردیی ناوەڕاست لاتینی',
+  dir: 'ltr',
+  script: 'Latin',
+};
+
+export default metadata;
