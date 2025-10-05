8dc278d7112e0573149740a64e098f949ac7169d
aren talb <aren.talb00@gmail.com>
2025-10-05 22:19:16 +0300

    feat(locale): add person module (name) for ku_ckb

--- src/locales/ku_ckb/person/index.ts
+++ src/locales/ku_ckb/person/index.ts
@@ -6,6 +6,7 @@ import type { PersonDefinition } from '../../..';
 import first_name from './first_name';
 import last_name from './last_name';
 import middle_name from './middle_name';
+import name_ from './name';
 import prefix from './prefix';
 import sex from './sex';
 
@@ -13,6 +14,7 @@ const person: PersonDefinition = {
   first_name,
   last_name,
   middle_name,
+  name: name_,
   prefix,
   sex,
 };
