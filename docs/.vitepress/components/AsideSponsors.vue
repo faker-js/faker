95b7cf4106129b7379d65815616c32d91438332a
Shinigami92 <chrissi92@hotmail.de>
2026-01-09 10:06:56 +0100

    fetch oc api

--- docs/.vitepress/components/AsideSponsors.vue
+++ docs/.vitepress/components/AsideSponsors.vue
@@ -2,7 +2,7 @@
 import type { Sponsors } from 'vitepress/dist/client/theme-default/components/VPSponsors.vue';
 import { VPDocAsideSponsors } from 'vitepress/theme';
 import type { Ref } from 'vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { bronze, gold, individuals, silver } from './sponsors.json';
 
 const sponsors: Ref<Sponsors[]> = ref([
@@ -11,6 +11,47 @@ const sponsors: Ref<Sponsors[]> = ref([
   { tier: 'Silver ($250)', size: 'mini', items: silver },
   { tier: 'Bronze ($100)', size: 'xmini', items: bronze },
 ]);
+
+onMounted(async () => {
+  const response = await fetch('https://api.opencollective.com/graphql/v2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      operationName: 'GetFakerJSDonations',
+      query: `query GetFakerJSDonations {
+  account(slug: "fakerjs") {
+    name
+    slug
+    members(role: BACKER, limit: 1000) {
+      totalCount
+      nodes {
+        account {
+          name
+          slug
+        }
+        since
+        totalDonations {
+          value
+          currency
+          valueInCents
+        }
+      }
+    }
+  }
+}
+`,
+      variables: {},
+    }),
+  }).then((res) => res.json());
+
+  const sorted = response.data.account.members.nodes.toSorted(
+    (a, b) => b.totalDonations.valueInCents - a.totalDonations.valueInCents
+  );
+
+  console.log(sorted.slice(0, 10));
+});
 </script>
 
 <template>
