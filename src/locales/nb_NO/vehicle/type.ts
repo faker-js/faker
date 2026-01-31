c72bb2bdbba9a6f9f6b01c95c6a31e7d3d00cb4e
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2026-01-31 12:24:42 +0100

    feat(locale): add Norwegian (nb_NO) vehicle type

---
+++ src/locales/nb_NO/vehicle/type.ts
@@ -0,0 +1,14 @@
+export default [
+  'Flerbruksbil',
+  'Halvkombi',
+  'Kabriolet',
+  'Kassebil',
+  'Kupé',
+  'Personbil',
+  'Pickup',
+  'SUV',
+  'Sedan',
+  'Stasjonsvogn',
+  'Varebil',
+  'Vogn',
+];
