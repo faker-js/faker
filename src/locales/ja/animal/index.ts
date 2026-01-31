4d2d9068e7129057c56aa2d2dc0f35cc80136dfc
Atsushi Kawamura <atsushi1230.sdi@gmail.com>
2026-01-31 10:45:59 +0900

    feat(locale): add Japanese cat breed definitions

--- src/locales/ja/animal/index.ts
+++ src/locales/ja/animal/index.ts
@@ -3,9 +3,11 @@
  * Run 'pnpm run generate:locales' to update.
  */
 import type { AnimalDefinition } from '../../..';
+import cat from './cat';
 import dog from './dog';
 
 const animal: AnimalDefinition = {
+  cat,
   dog,
 };
 
