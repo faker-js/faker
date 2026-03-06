057aa22a6be0e74e659326192889008c40043540
ST-DDT <ST-DDT@gmx.de>
2026-03-06 16:05:35 +0100

    refactor: transform word module

--- src/modules/word/sample.ts
+++ src/modules/word/sample.ts
@@ -2,6 +2,13 @@ import type { FakerCore } from '../../core';
 import { FakerError } from '../../errors/faker-error';
 import type { LengthStrategyType, NumberOrRange } from '../../utils/types';
 import { shuffle } from '../helpers/shuffle';
+import { adjective } from './adjective';
+import { adverb } from './adverb';
+import { conjunction } from './conjunction';
+import { interjection } from './interjection';
+import { noun } from './noun';
+import { preposition } from './preposition';
+import { verb } from './verb';
 
 /**
  * Returns a random word, that can be an adjective, adverb, conjunction, interjection, noun, preposition, or verb.
@@ -39,18 +46,18 @@ export function wordSample(
       } = {}
 ): string {
   const wordMethods = shuffle(fakerCore, [
-    this.adjective,
-    this.adverb,
-    this.conjunction,
-    this.interjection,
-    this.noun,
-    this.preposition,
-    this.verb,
-  ]);
+    adjective,
+    adverb,
+    conjunction,
+    interjection,
+    noun,
+    preposition,
+    verb,
+  ] satisfies Array<typeof wordSample>);
 
   for (const randomWordMethod of wordMethods) {
     try {
-      return randomWordMethod(options);
+      return randomWordMethod(fakerCore, options);
     } catch {
       // catch missing locale data potentially required by randomWordMethod
       continue;
