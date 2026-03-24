b8a63d6c8ca946b44f40fac8c7fe251043541ff4
Matt Mayer <matt@matthewmayer.co.uk>
2026-03-24 23:39:02 +0700

    multiline output in examples

--- src/modules/location/index.ts
+++ src/modules/location/index.ts
@@ -368,10 +368,25 @@ export class LocationModule extends SimpleLocationModule {
    * Generates a random localized full postal address, which may include a street address, secondary address, city, state, and zip code. To ensure you get locale-specific address formats, use a localized Faker instance.
    *
    * @example
-   * faker.location.postalAddress() // 'Apt. 980\n0917 O'Conner Estates\nWest Shannonview\nMichigan\n82180'
-   * fakerEN_US.location.postalAddress() // '0917 O'Conner Estates, Apt. 980\nWest Shannonview, MI 82180'
-   * fakerEN_GB.location.postalAddress() // '79 Bogan Corner\nCastle Zemlakborough\nDumfries and Galloway\nZH17 2SD'
-   * fakerZH_CN.location.postalAddress() // '广东省贵原市门路19号'
+   * faker.location.postalAddress()
+   * // 'Apt. 980
+   * // 0917 O'Conner Estates
+   * // West Shannonview
+   * // Michigan
+   * // 82180'
+   *
+   * fakerEN_US.location.postalAddress()
+   * // '0917 O'Conner Estates, Apt. 980
+   * // West Shannonview, MI 82180'
+   *
+   * fakerEN_GB.location.postalAddress()
+   * // '79 Bogan Corner
+   * // Castle Zemlakborough
+   * // Dumfries and Galloway
+   * // ZH17 2SD'
+   *
+   * fakerZH_CN.location.postalAddress()
+   * // '广东省贵原市门路19号'
    *
    * @since 10.5.0
    */
