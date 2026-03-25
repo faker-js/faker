71cdb47cd11df78de0b1ec01299721eb28baf04c
Matt Mayer <matt@matthewmayer.co.uk>
2026-03-25 21:42:38 +0700

    add de pattern

---
+++ src/locales/de/location/postal_address.ts
@@ -0,0 +1,4 @@
+export default [
+  '{{location.streetAddress}}\n{{location.secondaryAddress}}\n{{location.zipCode}} {{location.city}}',
+  '{{location.streetAddress}}\n{{location.zipCode}} {{location.city}}',
+];
