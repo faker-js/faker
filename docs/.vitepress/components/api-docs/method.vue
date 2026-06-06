3ac7b502a151fdc99cd0a2138ade5423802d2daf
ST-DDT <ST-DDT@gmx.de>
2026-06-06 20:20:58 +0200

    chore: adjust message

--- docs/.vitepress/components/api-docs/method.vue
+++ docs/.vitepress/components/api-docs/method.vue
@@ -43,7 +43,7 @@ function seeAlsoToUrl(see: string): string {
 
     <div v-if="experimental" class="warning custom-block">
       <p class="custom-block-title">Experimental</p>
-      <p>This method is experimental and may change in future versions.</p>
+      <p>This method is experimental and changes may not follow semantic versioning.</p>
     </div>
 
     <div v-html="description"></div>
