b3c291837845d31890eb0c0a5216118d7da8bd81
ST-DDT <ST-DDT@gmx.de>
2026-08-08 18:57:30 +0200

    Update src/modules/vehicle/module.ts

--- src/modules/vehicle/module.ts
+++ src/modules/vehicle/module.ts
@@ -2,7 +2,7 @@ import { ModuleBase } from '../../internal/module-base';
 
 // NHTSA 49 CFR § 565.15(c), Tables III and IV define the transliteration
 // values and position weights used here:
-// https://www.govinfo.gov/content/pkg/CFR-2025-title49-vol6/pdf/CFR-2025-title49-vol6-sec565-15.pdf
+// https://www.govinfo.gov/content/pkg/CFR-2024-title49-vol6/pdf/CFR-2024-title49-vol6-sec565-15.pdf
 const vinWeights = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2];
 
 const vinTransliteration: Record<string, number> = {
