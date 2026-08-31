a206b61b5a9c2bc1a113855bb913aa75b1646c96
Deshraj Verma <deshrajvermay9517@gmail.com>
2026-08-31 15:50:24 +0530

    docs(string): refine numeric error documentation

--- src/modules/string/module.ts
+++ src/modules/string/module.ts
@@ -492,8 +492,7 @@ export class StringModule extends SimpleModuleBase {
    * @param options.allowLeadingZeros Whether leading zeros are allowed or not. Defaults to `true`.
    * @param options.exclude An array of digits which should be excluded in the generated string. Defaults to `[]`.
    *
-   * @throws {FakerError} If no digits are available after applying `exclude`.
-   * @throws {FakerError} If `allowLeadingZeros` is `false` and `0` is the only available digit.
+   * @throws {FakerError} If no digits are available after applying `exclude` and `allowLeadingZeros`.
    *
    * @see faker.number.int(): For generating a number (within a range).
    *
