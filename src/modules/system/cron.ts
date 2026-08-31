7e8d18adf17905d4351e57ff442fbe7af2188c44
ST-DDT <ST-DDT@gmx.de>
2026-08-31 18:19:17 +0200

    chore: remove temp exports

--- src/modules/system/cron.ts
+++ src/modules/system/cron.ts
@@ -3,8 +3,7 @@ import { boolean } from '../datatype/boolean';
 import { arrayElement } from '../helpers/array-element';
 import { int } from '../number/int';
 
-// Temp export
-export const CRON_DAY_OF_WEEK = [
+const CRON_DAY_OF_WEEK = [
   'SUN',
   'MON',
   'TUE',
