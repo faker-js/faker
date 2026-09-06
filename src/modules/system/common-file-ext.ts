69d8cc7517e37834a1b1b0e65996e5fcd89dd28c
ST-DDT <ST-DDT@gmx.de>
2026-09-06 13:09:44 +0200

    chore: run transform script (dirty)

---
+++ src/modules/system/common-file-ext.ts
@@ -0,0 +1,19 @@
+import type { FakerCore } from '../../core';
+import { arrayElement } from '../helpers/array-element';
+import { fileExt } from './file-ext';
+
+/**
+ * Returns a commonly used file extension.
+ *
+ * @param fakerCore The FakerCore to use.
+ *
+ * @example
+ * commonFileExt(fakerCore) // 'gif'
+ *
+ * @since 11.0.0
+ *
+ * @experimental
+ */
+export function commonFileExt(fakerCore: FakerCore): string {
+  return fileExt(fakerCore, arrayElement(fakerCore, commonMimeTypes));
+}
