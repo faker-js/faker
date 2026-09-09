25dfad0255780750ae61bbaab78aa8150f6e05c9
ST-DDT <ST-DDT@gmx.de>
2026-09-09 23:11:20 +0200

    docs(helpers): refer to elements instead of strings in uniqueArray

--- src/modules/helpers/unique-array.ts
+++ src/modules/helpers/unique-array.ts
@@ -2,8 +2,8 @@ import type { FakerCore } from '../../core';
 import { shuffle } from './shuffle';
 
 /**
- * Takes an array of strings or function that returns a string
- * and outputs a unique array of strings based on that source.
+ * Takes an array of elements or function that returns an element
+ * and outputs a unique array of elements based on that source.
  * This method does not store the unique state between invocations.
  *
  * If there are not enough unique values to satisfy the length, if
@@ -14,7 +14,7 @@ import { shuffle } from './shuffle';
  * @template T The type of the elements.
  *
  * @param fakerCore The FakerCore to use.
- * @param source The strings to choose from or a function that generates a string.
+ * @param source The elements to choose from or a function that generates an element.
  * @param length The number of elements to generate.
  *
  * @example
