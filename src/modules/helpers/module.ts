088ad8a7fc68af9e67f43047ff46d3e88f6d09d5
ST-DDT <ST-DDT@gmx.de>
2026-09-10 08:27:49 +0000

    docs(helpers): refer to elements instead of strings in uniqueArray (#4083)

--- src/modules/helpers/module.ts
+++ src/modules/helpers/module.ts
@@ -229,8 +229,8 @@ export class SimpleHelpersModule extends SimpleModuleBase {
   }
 
   /**
-   * Takes an array of strings or function that returns a string
-   * and outputs a unique array of strings based on that source.
+   * Takes an array of elements or function that returns an element
+   * and outputs a unique array of elements based on that source.
    * This method does not store the unique state between invocations.
    *
    * If there are not enough unique values to satisfy the length, if
@@ -240,7 +240,7 @@ export class SimpleHelpersModule extends SimpleModuleBase {
    *
    * @template T The type of the elements.
    *
-   * @param source The strings to choose from or a function that generates a string.
+   * @param source The elements to choose from or a function that generates an element.
    * @param length The number of elements to generate.
    *
    * @example
