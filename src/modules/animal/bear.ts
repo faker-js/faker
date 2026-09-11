0e807e161336acf381ef4207af4270fec2d2a16a
ST-DDT <ST-DDT@gmx.de>
2026-09-11 18:20:10 +0200

    revert: previous error

--- src/modules/animal/bear.ts
+++ src/modules/animal/bear.ts
@@ -14,6 +14,5 @@ import { arrayElement } from '../helpers/array-element';
  * @experimental
  */
 export function bear(fakerCore: FakerCore): string {
-
   return arrayElement(fakerCore, fakerCore.locale.animal.bear);
 }
